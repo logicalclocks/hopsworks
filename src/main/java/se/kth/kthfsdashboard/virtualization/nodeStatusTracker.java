eda5560682c02c0c70b8ce2504b448d026becc85
alorlea <a.lorenteleal@gmail.com>
2013-06-10 16:02:27 +0200

    added new cluster and statustracker

---
+++ src/main/java/se/kth/kthfsdashboard/virtualization/nodeStatusTracker.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kth.kthfsdashboard.virtualization;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import org.jclouds.compute.domain.ExecResponse;
+import org.jclouds.compute.domain.NodeMetadata;
+
+/**
+ *
+ * @author Alberto Lorente Leal <albll@kth.se>
+ */
+public class nodeStatusTracker implements Runnable {
+
+    private final NodeMetadata launchingNode;
+    private final CountDownLatch latch;
+    private final CopyOnWriteArraySet<NodeMetadata> pendingNodes;
+    private ListenableFuture<ExecResponse> future;
+
+    public nodeStatusTracker(NodeMetadata launchingNode, CountDownLatch latch,
+            CopyOnWriteArraySet<NodeMetadata> pendingNodes, ListenableFuture<ExecResponse> future) {
+        this.launchingNode = launchingNode;
+        this.latch = latch;
+        this.pendingNodes = pendingNodes;
+        this.future = future;
+    }
+
+    @Override
+    public void run() {
+        try {
+            ExecResponse contents = future.get();
+            latch.countDown();
+            //...process 
+            pendingNodes.remove(launchingNode);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted" + e);
+        } catch (ExecutionException e) {
+            System.out.println("Interrupted" + e.getCause());
+        }
+    }
+}
