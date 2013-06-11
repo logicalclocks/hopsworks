cfeb085cfd094a8dcb2352bc05b800603a7cbab7
Hamidreza Afzali <afzali@kth.se>
2013-06-11 11:35:24 +0200

    Supporting both http and https for graph REST URLs

--- src/main/java/se/kth/kthfsdashboard/rest/resources/CollectdResource.java
+++ src/main/java/se/kth/kthfsdashboard/rest/resources/CollectdResource.java
@@ -5,7 +5,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.security.RolesAllowed;
