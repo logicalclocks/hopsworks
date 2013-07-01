df4a740b019f1e73eca8efb45c8e1be8ae7148bb
Hamidreza Afzali <afzali@kth.se>
2013-07-01 17:28:25 +0200

    Error message for RRD charts

--- src/main/java/se/kth/kthfsdashboard/rest/resources/CollectdResource.java
+++ src/main/java/se/kth/kthfsdashboard/rest/resources/CollectdResource.java
@@ -1,10 +1,22 @@
 package se.kth.kthfsdashboard.rest.resources;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.font.FontRenderContext;
+import java.awt.font.LineBreakMeasurer;
+import java.awt.font.TextAttribute;
+import java.awt.font.TextLayout;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.AttributedCharacterIterator;
+import java.text.AttributedString;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.security.RolesAllowed;
@@ -13,6 +25,7 @@ import javax.imageio.ImageIO;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.jclouds.ec2.domain.Image;
 import se.kth.kthfsdashboard.util.CollectdTools;
 
 /**
@@ -49,17 +62,56 @@ public class CollectdResource {
            @QueryParam("n") int n) throws InterruptedException, IOException {
 
       CollectdTools collectdTools = new CollectdTools();
-      System.err.println(chartType);
+      ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
       try {
-         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
          InputStream inputStream = collectdTools.getGraphStream(chartType, host, plugin, pluginInstance, type, typeInstance, ds, start, end, n);
          BufferedImage image = ImageIO.read(inputStream);
          ImageIO.write(image, "png", byteArrayOutputStream);
          byte[] imageData = byteArrayOutputStream.toByteArray();
          return Response.ok(new ByteArrayInputStream(imageData)).build();
       } catch (Exception e) {
-         logger.log(Level.SEVERE, "CollectdResource >> Image == null : check rrd file for {0}: {1}/{2} @ {3} " , new Object[] {chartType, plugin, pluginInstance, host});
-         return Response.status(Response.Status.NOT_FOUND).build();
+         logger.log(Level.SEVERE, "CollectdResource >> Image == null : check rrd file for {0}: {1}/{2} @ {3} ", new Object[]{chartType, plugin, type, host});
+         String msg = String.format("Graph not available. Check RRD files for %s: %s / %s @ %s ", chartType, plugin, type, host);
+         BufferedImage image = createErrorImage(msg);
+         ImageIO.write(image, "png", byteArrayOutputStream);
+         byte[] imageData = byteArrayOutputStream.toByteArray();
+         return Response.ok(new ByteArrayInputStream(imageData)).build();
+//         return Response.status(Response.Status.NOT_FOUND).build();
+      }
+   }
+
+   private BufferedImage createErrorImage(String msg) {
+      int w = 350;
+      int h = 200;
+      BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+      Graphics2D g2d = img.createGraphics();
+      g2d.setPaint(Color.black);
+      g2d.setFont(new Font("Arial", Font.PLAIN, 12));
+      FontMetrics fm = g2d.getFontMetrics();
+      int x = 5;
+      int y = fm.getHeight();
+      drawString(g2d, x, y, w, h, msg);
+      g2d.dispose();
+      return img;
+   }
+
+   private void drawString(Graphics2D g2d, int x1, int y1, int x2, int y2, String txt) {
+      float interline = 1;
+      float width = x2 - x1;
+      AttributedString as = new AttributedString(txt);
+      as.addAttribute(TextAttribute.FOREGROUND, g2d.getPaint());
+      as.addAttribute(TextAttribute.FONT, g2d.getFont());
+      AttributedCharacterIterator aci = as.getIterator();
+      FontRenderContext frc = new FontRenderContext(null, true, false);
+      LineBreakMeasurer lbm = new LineBreakMeasurer(aci, frc);
+      while (lbm.getPosition() < txt.length()) {
+         TextLayout tl = lbm.nextLayout(width);
+         y1 += tl.getAscent();
+         tl.draw(g2d, x1, y1);
+         y1 += tl.getDescent() + tl.getLeading() + (interline - 1.0f) * tl.getAscent();
+         if (y1 > y2) {
+            break;
+         }
       }
    }
 }
