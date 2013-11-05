1bb98b1ca15bf3c65961e6936f6cd4a85a84ac01
alorlea <a.lorenteleal@gmail.com>
2013-11-05 17:48:30 +0100

    added workflow table with in memory data

---
+++ src/main/java/se/kth/kthfsdashboard/wf/Workflow.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kth.kthfsdashboard.wf;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Alberto Lorente Leal <albll@kth.se>
+ */
+public class Workflow implements Serializable{
+    private String owner;
+    private String workflowName;
+    private String workflowDate;
+    private String workflowTags;
+
+    public Workflow() {
+    }
+
+    public Workflow(String owner, String workflowName, String workflowDate, String workflowContents) {
+        this.owner = owner;
+        this.workflowName = workflowName;
+        this.workflowDate = workflowDate;
+        this.workflowTags = workflowContents;
+    }
+    
+    
+
+    
+
+    public String getWorkflowTags() {
+        return workflowTags;
+    }
+
+    public void setWorkflowTags(String workflowContents) {
+        this.workflowTags = workflowContents;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getWorkflowName() {
+        return workflowName;
+    }
+
+    public void setWorkflowName(String workflowName) {
+        this.workflowName = workflowName;
+    }
+
+    public String getWorkflowDate() {
+        return workflowDate;
+    }
+
+    public void setWorkflowDate(String workflowDate) {
+        this.workflowDate = workflowDate;
+    }
+    
+      
+    
+    
+}
