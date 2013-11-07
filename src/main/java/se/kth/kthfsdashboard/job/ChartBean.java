312d512768bb924fc766ac426dc4003cb183ad14
alorlea <a.lorenteleal@gmail.com>
2013-11-07 11:21:46 +0100

    added dummy chart data, include dependencies for cuneiform and logview to execute workflow. Note: the .jar files are fetched from local maven repository so you need to have them locally

---
+++ src/main/java/se/kth/kthfsdashboard/job/ChartBean.java
@@ -0,0 +1,47 @@
+package se.kth.kthfsdashboard.job;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import org.primefaces.model.chart.CartesianChartModel;
+import org.primefaces.model.chart.ChartSeries;
+
+@ManagedBean
+@ViewScoped
+public class ChartBean implements Serializable {
+
+    private CartesianChartModel categoryModel;
+
+    public ChartBean() {
+        createCategoryModel();
+    }
+
+    public CartesianChartModel getCategoryModel() {
+        return categoryModel;
+    }
+
+    private void createCategoryModel() {
+        categoryModel = new CartesianChartModel();
+
+        ChartSeries boys = new ChartSeries();
+        boys.setLabel("Boys");
+
+        boys.set("2004", 120);
+        boys.set("2005", 100);
+        boys.set("2006", 44);
+        boys.set("2007", 150);
+        boys.set("2008", 25);
+
+        ChartSeries girls = new ChartSeries();
+        girls.setLabel("Girls");
+
+        girls.set("2004", 52);
+        girls.set("2005", 60);
+        girls.set("2006", 110);
+        girls.set("2007", 135);
+        girls.set("2008", 120);
+
+        categoryModel.addSeries(boys);
+        categoryModel.addSeries(girls);
+    }
+}
