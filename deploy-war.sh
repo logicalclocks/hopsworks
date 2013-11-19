0d2019c1f12aaebbcd8ee54fd52dced9aa6229ec
Jim Dowling <jdowling@sics.se>
2013-11-19 17:09:11 +0100

    updated deploy script to use snurran

--- deploy-war.sh
+++ deploy-war.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #mvn war:war
 mvn clean install
-scp target/kthfs-dashboard.war glassfish@lucan.sics.se:/var/www/kthfs
+scp target/kthfs-dashboard.war glassfish@snurran.sics.se:/var/www/hops
