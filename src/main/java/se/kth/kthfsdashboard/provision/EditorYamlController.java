bacd20e27ff17bca7cdbe7bfe985f3fda9481b78
alorlea <a.lorenteleal@gmail.com>
2013-11-27 17:21:30 +0100

    added functional version of the editor

--- src/main/java/se/kth/kthfsdashboard/provision/EditorYamlController.java
+++ src/main/java/se/kth/kthfsdashboard/provision/EditorYamlController.java
@@ -5,30 +5,33 @@
 package se.kth.kthfsdashboard.provision;
 
 import java.io.Serializable;
-import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import se.kth.kthfsdashboard.virtualization.clusterparser.ClusterEntity;
+import se.kth.kthfsdashboard.virtualization.clusterparser.ClusterFacade;
 
 /**
  *
  * @author Alberto Lorente Leal <albll@kth.se>
  */
 @ManagedBean
-@RequestScoped
-public class YAMLEditorController implements Serializable {
+@SessionScoped
+public class EditorYamlController implements Serializable {
 
     private static final long serialVersionUID = 20131126L;
     private ClusterEntity entity;
     private String content;
     private String mode = "yaml";
+    @EJB
+    private ClusterFacade clusterEJB;
 
-    public YAMLEditorController() {
-      
+    public EditorYamlController() {
     }
-    @PostConstruct
-    public void init(){
-        this.content=entity.getYamlContent();
+
+    
+    public void init() {
+        this.content = entity.getYamlContent();
     }
 
     public String getContent() {
@@ -46,5 +49,18 @@ public class YAMLEditorController implements Serializable {
     public void setMode(final String mode) {
         this.mode = mode;
     }
+
+    public ClusterEntity getEntity() {
+        return entity;
+    }
+
+    public void setEntity(ClusterEntity entity) {
+        this.entity = entity;
+    }
     
+    public String saveChanges(){
+        entity.setYamlContent(content);
+        clusterEJB.updateCluster(entity);
+        return "addcluster";
+    }
 }
