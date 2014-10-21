dbec9a70a7082d14f37ea038c4e9d35c030dc96e
Jim Dowling <jdowling@kth.se>
2014-10-21 09:25:59 +0200

    ajax updating of browser

--- src/main/java/se/kth/bbc/study/fb/FilesMB.java
+++ src/main/java/se/kth/bbc/study/fb/FilesMB.java
@@ -5,9 +5,13 @@
  */
 package se.kth.bbc.study.fb;
 
+import com.google.common.collect.Lists;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -17,18 +21,24 @@ import javax.faces.bean.SessionScoped;
  */
 @ManagedBean(name = "FilesMB")
 @SessionScoped
-public class FilesMB implements Serializable{
+public class FilesMB implements Serializable {
 
     List<FileInfo> files = new ArrayList<>();
     FileInfo root = new FileInfo("/", "root", FileInfo.Status.READY, 0, true, null);
     FileInfo cwd;
 
+    private class BadPath extends Exception {
+
+        public BadPath(String msg) {
+            super(msg);
+        }
+    }
 
     public FilesMB() {
         this.cwd = this.root;
         this.cwd.addChild(new FileInfo("tmp", "jim", FileInfo.Status.READY, 1024, false, this.cwd));
     }
-    
+
     public List<FileInfo> getFiles() {
         return cwd.getChildren();
     }
@@ -36,7 +46,7 @@ public class FilesMB implements Serializable{
     public void setFiles(List<FileInfo> files) {
         this.files = files;
     }
-    
+
     public void cdUp() {
         if (!cwd.isRoot()) {
             // nullify object reference to prevent mem leak
@@ -45,7 +55,7 @@ public class FilesMB implements Serializable{
             cwd = cwd.getParent();
         }
     }
-    
+
     public void cdDown(String name) {
 
         for (FileInfo f : cwd.getChildren()) {
@@ -54,5 +64,67 @@ public class FilesMB implements Serializable{
             }
         }
     }
-    
+
+    private boolean isRoot() {
+        if (cwd.equals(root)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param components string for path to parse
+     * @param path empty to begin with
+     * @param origCwd cwd when calling this method and still cwd when it returns
+     * @return list of path components, starting with root.
+     */
+    private List<FileInfo> getPathComponents(List<String> components, List<FileInfo> path, FileInfo origCwd)
+            throws BadPath {
+        if (components.size() < 1) {
+            throw new BadPath("Path was empty");
+        }
+        if (components.size() == 1) { //base case
+            path.add(this.cwd);
+            this.cwd = origCwd;
+            return Lists.reverse(path); // put the root at the start of the list
+        }
+        if (path.isEmpty()) {
+            this.cwd = this.root;
+        }
+        path.add(this.cwd);
+        components.remove(0);
+        return getPathComponents(components, path, origCwd);
+    }
+
+    private int distanceFromRoot(FileInfo f, int d, FileInfo origCwd) {
+        if (isRoot()) { // base case
+            this.cwd = origCwd;
+            d++;
+            return d;
+        }
+        d++;
+        f = f.getParent();
+        return distanceFromRoot(f, d, origCwd);
+    }
+
+    /**
+     * 
+     * @param name valid path
+     */
+    public void cd(String name) {
+        String[] p = name.split("/");
+        List<String> pathComponents = Arrays.asList(p);
+        try {
+            List<FileInfo> path = getPathComponents(pathComponents, new ArrayList<FileInfo>(), this.cwd);
+            // TODO: Do not allow user to change to arbitrary directory outside the project
+            
+            // Change cwd to last element in the path
+            this.cwd = path.get(path.size()-1);
+        } catch (BadPath ex) {
+            Logger.getLogger(FilesMB.class.getName()).log(Level.SEVERE, null, ex);
+            // TODO: Faces msg to user here.
+        }
+    }
+
 }
