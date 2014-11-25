69f4cf7cd237d1d1d3d5473393275b28b13f7cb5
stig <stig@stig-OptiPlex-7010>
2014-11-25 11:39:25 +0100

    Fixed Constants.java

--- src/main/java/se/kth/bbc/lims/Constants.java
+++ src/main/java/se/kth/bbc/lims/Constants.java
@@ -12,4 +12,7 @@ public class Constants {
 
     public static final String UPLOAD_DIR = server.equals("LOCAL") ? "/home/stig/tst" : "/tmp";
     public static final String NAMENODE_URI = server.equals("LOCAL") ? "hdfs://localhost:8020":"hdfs://snurran.sics.se:9999";
+    
+    public static final String LOCAL_APPMASTER_DIR = server.equals("LOCAL") ? "/home/stig/tst/appMaster" : "/tmp/appMaster";
+    public static final String LOCAL_EXTRA_DIR = server.equals("LOCAL") ? "/home/stig/tst/extraFiles" : "/tmp/extraFiles";
 }
