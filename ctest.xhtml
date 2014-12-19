53f101d2a5e964d9ecf8c567a9b82a08311e0928
Ali Gholami <gholami@sics.se>
2014-12-19 17:19:33 +0100

    Move some ua file to unprotected zone

---
+++ ctest.xhtml
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>TODO supply a title</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body>
+        <div>TODO write content</div>
+    </body>
+</html>
---
+++ src/main/java/se/kth/bbc/security/auth/totp/LoginFilter.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kth.bbc.security.auth.totp;
+
+/**
+ *
+ * @author Ali Gholami <gholami@pdc.kth.se>
+ */
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        // if user is logged in redirect to LIMS first page 
+        // otherwise continue 
+        if (request.getRemoteUser() != null) {
+            String contextPath = ((HttpServletRequest) request).getContextPath();
+            response.sendRedirect(contextPath + "/bbc/lims/index.xhtml");
+        } else {
+            chain.doFilter(req, res); 
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
