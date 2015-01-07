d21df8dc77c5d5c0b287df83c2bb6db8d39b7a50
Stig Viaene <stigviaene@sics.se>
2015-01-07 18:33:53 +0100

    Cleaned up Constants.java

--- src/main/java/se/kth/bbc/lims/Constants.java
+++ src/main/java/se/kth/bbc/lims/Constants.java
@@ -8,24 +8,25 @@ package se.kth.bbc.lims;
  */
 public class Constants {
 
-    public static final String server = "LOCAL";
-    //public static final String server = "SNURRAN";
+    //public static final String server = "LOCAL";
+    public static final String server = "SNURRAN";
 
+    //Staging folder for files to be uploaded in
     public static final String UPLOAD_DIR = server.equals("LOCAL") ? "/home/stig/tst" : "/tmp";
     
-    public static final String LOCAL_APPMASTER_DIR = server.equals("LOCAL") ? "/home/stig/tst/appMaster" : "/tmp/appMaster";
-    public static final String LOCAL_EXTRA_DIR = server.equals("LOCAL") ? "/home/stig/tst/extraFiles" : "/tmp/extraFiles";
-    
+    //Staging folder for upload of files used in job execution
     public static final String JOB_UPLOAD_DIR = server.equals("LOCAL") ? "/home/stig/tst/jobs" : "/tmp/jobs";
 
-    public static final String FLINK_CONF_DIR = server.equals("LOCAL") ? "/home/stig/projects/flink-yarn-0.7.0-incubating/conf/.yarn-properties" : "/home/glassfish/stig/flink-0.8-incubating-SNAPSHOT/conf/.yarn-properties";
-
+    //Local path to the hiway jar
     public static final String HIWAY_JAR_PATH = "LOCAL".equals(server)? "/srv/hiway-0.2.0-SNAPSHOT/hiway-core-0.2.0-SNAPSHOT.jar" : "/home/glassfish/stig/hiway-core-0.2.0-SNAPSHOT.jar";
     
+    //User under which yarn is run
     public static final String YARN_USER = "LOCAL".equals(server)? "stig":"glassfish";
     
+    //Relative output path (within hdfs study folder) which to write cuneiform output to
     public static final String CUNEIFORM_DEFAULT_OUTPUT_PATH = "Cuneiform/Output/";
     
+    //Default configuration locations
     public static final String DEFAULT_HADOOP_CONF_DIR = "/srv/hadoop/etc/hadoop/";
     public static final String DEFAULT_YARN_CONF_DIR = "/srv/hadoop/etc/hadoop/";
 }
