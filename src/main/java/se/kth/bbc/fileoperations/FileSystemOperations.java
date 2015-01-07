0898d08c01d54aa1143dcde7f86b2160a4d691b2
Stig Viaene <stigviaene@sics.se>
2015-01-07 17:19:06 +0100

    Read fs location from conf file

--- src/main/java/se/kth/bbc/fileoperations/FileSystemOperations.java
+++ src/main/java/se/kth/bbc/fileoperations/FileSystemOperations.java
@@ -1,5 +1,6 @@
 package se.kth.bbc.fileoperations;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
@@ -22,84 +23,102 @@ import se.kth.bbc.lims.Constants;
 public class FileSystemOperations {
 
     //TODO: use fs.copyFromLocalFile
+  private static final Logger logger = Logger.getLogger(
+          FileSystemOperations.class.getName());
+  public static final String DIR_ROOT = "Projects";
+  public static final String DIR_SAMPLES = "Samples";
+  public static final String DIR_CUNEIFORM = "Cuneiform";
+  public static final String DIR_RESULTS = "Results";
+  public static final String DIR_BAM = "bam";
+  public static final String DIR_SAM = "sam";
+  public static final String DIR_FASTQ = "fastq";
+  public static final String DIR_FASTA = "fasta";
+  public static final String DIR_VCF = "vcf";
+
+  /**
+   * Copy a file to HDFS. The file will end up at <i>location</i>. The
+   * InputStream represents the file.
+   *
+   * @param location Location to which the file should be copied. Includes the
+   * filename.
+   * @param is The inputstream representing the file.
+   * @throws IOException
+   * @throws URISyntaxException
+   */
+  public void copyToHDFS(Path location, InputStream is) throws IOException,
+          URISyntaxException {
+
+    // Get the filesystem
+    FileSystem fs = getFs();
+
+    // Write file
+    if (!fs.exists(location)) {
+      FSDataOutputStream os = fs.create(location, false);
+      IOUtils.copyBytes(is, os, 131072, true); //TODO: check what this 131072 means...
+    }
+  }
+
+  /**
+   * Get an input stream for the file at path <i>location</i>.
+   *
+   * @param location The location of the file.
+   * @return An InputStream for the file.
+   */
+  public InputStream getInputStream(Path location) throws IOException {
+    FileSystem fs = getFs();
+    return fs.open(location, 1048576); //TODO: undo hard coding of weird constant here...
+  }
+
+  /**
+   * Create a new folder on the given path.
+   *
+   * @param location The path to the new folder, its name included.
+   * @return True if successful.
+   */
+  public boolean mkdir(Path location) throws IOException {
+    FileSystem fs = getFs();
+    return fs.mkdirs(location, null);
+  }
+
+  /**
+   * Delete a file or directory form the file system.
+   *
+   * @param location The location of file or directory to be removed.
+   * @param recursive If true, a directory will be removed with all its
+   * children.
+   * @return True if the operation is successful.
+   * @throws IOException
+   */
+  public boolean rm(Path location, boolean recursive) throws IOException {
+    FileSystem fs = getFs();
+    boolean retVal = fs.delete(location, recursive);
+    return retVal;
+  }
+
+  private FileSystem getFs() throws IOException {
     
-    private static final String nameNodeURI = Constants.NAMENODE_URI;
-    private static final Logger logger = Logger.getLogger(FileSystemOperations.class.getName());
-    public static final String DIR_ROOT = "Projects";
-    public static final String DIR_SAMPLES = "Samples";
-    public static final String DIR_CUNEIFORM = "Cuneiform";
-    public static final String DIR_RESULTS = "Results";
-    public static final String DIR_BAM = "bam";
-    public static final String DIR_SAM = "sam";
-    public static final String DIR_FASTQ = "fastq";
-    public static final String DIR_FASTA = "fasta";
-    public static final String DIR_VCF = "vcf";
-
-    /**
-     * Copy a file to HDFS. The file will end up at <i>location</i>. The
-     * InputStream represents the file.
-     *
-     * @param location Location to which the file should be copied. Includes the
-     * filename.
-     * @param is The inputstream representing the file.
-     * @throws IOException
-     * @throws URISyntaxException
-     */
-    public void copyToHDFS(Path location, InputStream is) throws IOException, URISyntaxException {
-
-        // Get the filesystem
-        Configuration conf = new Configuration();
-        conf.set("fs.defaultFS", nameNodeURI);
-        FileSystem fs = FileSystem.get(conf);
-
-        // Write file
-        if (!fs.exists(location)) {
-            FSDataOutputStream os = fs.create(location, false);
-            IOUtils.copyBytes(is, os, 131072, true); //TODO: check what this 131072 means...
-        }
+    String coreConfDir = System.getenv("HADOOP_CONF_DIR");
+    //If still not found: throw exception
+    if (coreConfDir == null) {
+      logger.log(Level.WARNING, "No configuration path set, using default: "+Constants.DEFAULT_CONF_PATH);
+      coreConfDir = Constants.DEFAULT_CONF_PATH;
     }
 
-    /**
-     * Get an input stream for the file at path <i>location</i>.
-     *
-     * @param location The location of the file.
-     * @return An InputStream for the file.
-     */
-    public InputStream getInputStream(Path location) throws IOException {
-        Configuration conf = new Configuration();
-        conf.set("fs.defaultFS", nameNodeURI);
-        FileSystem fs = FileSystem.get(conf);
-        return fs.open(location, 1048576); //TODO: undo hard coding of weird constant here...
+    //Get the configuration file at found path
+    Path confPath = new Path(coreConfDir);
+    File confFile = new File(coreConfDir , "core-site.xml");
+    if (!confFile.exists()) {
+      logger.log(Level.SEVERE, "Unable to locate configuration file in {0}",
+              confFile);
+      throw new IllegalStateException("No conf file");
     }
 
-    /**
-     * Create a new folder on the given path.
-     *
-     * @param location The path to the new folder, its name included.
-     * @return True if successful.
-     */
-    public boolean mkdir(Path location) throws IOException {
-        Configuration conf = new Configuration();
-        conf.set("fs.defaultFS", nameNodeURI);
-        FileSystem fs = FileSystem.get(conf);
-        return fs.mkdirs(location, null);
-    }
-
-    /**
-     * Delete a file or directory form the file system.
-     *
-     * @param location The location of file or directory to be removed.
-     * @param recursive If true, a directory will be removed with all its
-     * children.
-     * @return True if the operation is successful.
-     * @throws IOException
-     */
-    public boolean rm(Path location, boolean recursive) throws IOException {
-        Configuration conf = new Configuration();
-        conf.set("fs.defaultFS", nameNodeURI);
-        FileSystem fs = FileSystem.get(conf);
-        boolean retVal = fs.delete(location, recursive);
-        return retVal;
-    }
+    //Set the Configuration object for the returned YarnClient
+    Path yarnPath = new Path(confFile.getAbsolutePath());
+    Configuration conf = new Configuration();
+    conf.addResource(yarnPath);    
+    FileSystem fs = FileSystem.get(conf);
+    return fs;
+  }
 
 }
