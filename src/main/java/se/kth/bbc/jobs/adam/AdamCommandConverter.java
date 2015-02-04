52c3987cf0689ff550772065588e62451e8b51b2
Stig Viaene <stigviaene@sics.se>
2015-02-04 10:11:55 +0100

    Fixed converter

--- src/main/java/se/kth/bbc/jobs/adam/AdamCommandConverter.java
+++ src/main/java/se/kth/bbc/jobs/adam/AdamCommandConverter.java
@@ -3,6 +3,7 @@ package se.kth.bbc.jobs.adam;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
 
 /**
@@ -15,18 +16,22 @@ public class AdamCommandConverter implements Converter {
   @Override
   public Object getAsObject(FacesContext context, UIComponent component,
           String value) {
-    System.out.println("Called getAsObject "+value);
+    if(value == null || value.isEmpty()){
+      return null;
+    }
     return AdamCommand.getFromCommand(value);
   }
 
   @Override
   public String getAsString(FacesContext context, UIComponent component,
           Object value) {
+    if(value == null || "".equals(value)){
+      return "";
+    }
     if (!(value instanceof AdamCommand)) {
-      throw new IllegalArgumentException("Object " + value
+      throw new ConverterException("Object " + value
               + " is not of the expected type AdamCommand.");
     }
-    System.out.println("Called getAsString "+value);
     return ((AdamCommand) value).getCommand();
   }
 
