3ed9d3a9ede34cbdba0114a1b5004e9daf44b03f
Jim Dowling <jdowling@sics.se>
2015-03-13 08:52:16 +0100

    admin/admin added

--- rows.sql
+++ rows.sql
@@ -14,7 +14,7 @@ INSERT INTO `BBCGroup` VALUES
     password: admin
  */;
 insert into USERS (uid, username, password, email, fname, lname, activated, status, secret, security_question, security_answer, yubikey_user)
-values ('10000', 'meb10000', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918','admin','Jim','Dowling',
+values ('10000', 'meb10000', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918','admin','Admin','Admin',
  CURRENT_TIMESTAMP, 4, 'V3WBPS4G2WMQ53VA', 'friend', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', -1);
 
 /*
