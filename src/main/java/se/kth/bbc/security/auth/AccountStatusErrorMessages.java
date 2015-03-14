70bae12695f371188e0fcadb9047b370ba1095b2
Ali Gholami <gholami@sics.se>
2015-03-14 16:08:28 +0100

    Erorr messages fixed

--- src/main/java/se/kth/bbc/security/auth/AccountStatusErrorMessages.java
+++ src/main/java/se/kth/bbc/security/auth/AccountStatusErrorMessages.java
@@ -9,7 +9,7 @@ public class AccountStatusErrorMessages {
 
     public final static String INCCORCT_CREDENTIALS ="The email or password is incorrect.";
     
-    public final static String USER_NOT_FOUND ="No account found for this email address.";
+    public final static String USER_NOT_FOUND ="No account found.";
     
     public final static String BLOCKED_ACCOUNT ="This account is blocked.";
     
@@ -17,11 +17,11 @@ public class AccountStatusErrorMessages {
     
     public final static String INVALID_SEQ_ANSWER ="Inccorrect answer.";
     
-    public final static String DEACTIVATED_ACCOUNT ="This account is not in use anymore.";
+    public final static String DEACTIVATED_ACCOUNT ="This account is deactivated.";
     
     public final static String INCORRECT_PIN ="PIN must be 6 charachters.";
     
-    public final static String PIN_REQUIERMENTS ="PPIN must be numeric.";
+    public final static String PIN_REQUIERMENTS ="PIN must be numeric.";
     
     public final static String PASSWORD_REQUIREMNTS="Password must contain at least 6 characters.";
     
