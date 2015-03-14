86d312fc3d2ce2b18bd88afbafe93275f93cb258
Ali Gholami <gholami@sics.se>
2015-03-14 16:04:01 +0100

    Security error messages

---
+++ src/main/java/se/kth/bbc/security/auth/AccountStatusErrorMessages.java
@@ -0,0 +1,28 @@
+package se.kth.bbc.security.auth;
+
+/**
+ * This class contains the error messages that user get upon wrong authentication.
+ * 
+ * @author Ali Gholami <gholami@pdc.kth.se>
+ */
+public class AccountStatusErrorMessages {
+
+    public final static String INCCORCT_CREDENTIALS ="The email or password is incorrect.";
+    
+    public final static String USER_NOT_FOUND ="No account found for this email address.";
+    
+    public final static String BLOCKED_ACCOUNT ="This account is blocked.";
+    
+    public final static String INACTIVE_ACCOUNT ="This account is not activated.";
+    
+    public final static String INVALID_SEQ_ANSWER ="Inccorrect answer.";
+    
+    public final static String DEACTIVATED_ACCOUNT ="This account is not in use anymore.";
+    
+    public final static String INCORRECT_PIN ="PIN must be 6 charachters.";
+    
+    public final static String PIN_REQUIERMENTS ="PPIN must be numeric.";
+    
+    public final static String PASSWORD_REQUIREMNTS="Password must contain at least 6 characters.";
+    
+}
