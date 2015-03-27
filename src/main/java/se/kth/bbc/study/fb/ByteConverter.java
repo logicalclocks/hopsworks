c62e2ce99093c20458eec4f707adc991eb0687de
Stig Viaene <stigviaene@sics.se>
2015-03-27 14:34:44 +0100

    Seem to have fixed DB transaction issue

--- src/main/java/se/kth/bbc/study/fb/ByteConverter.java
+++ src/main/java/se/kth/bbc/study/fb/ByteConverter.java
@@ -1,34 +1,34 @@
 package se.kth.bbc.study.fb;
 
-import org.eclipse.persistence.mappings.DatabaseMapping;
-import org.eclipse.persistence.mappings.converters.Converter;
-import org.eclipse.persistence.sessions.Session;
+//import org.eclipse.persistence.mappings.DatabaseMapping;
+//import org.eclipse.persistence.mappings.converters.Converter;
+//import org.eclipse.persistence.sessions.Session;
 
 /**
  * Convert between MySQL bit(8) and Java byte.
  * <p>
  * @author stig
  */
-public class ByteConverter implements Converter {
+public class ByteConverter/* implements Converter*/ {
 
-  @Override
-  public Object convertObjectValueToDataValue(Object o, Session sn) {
-    return Byte.MIN_VALUE;
-  }
-
-  @Override
-  public Object convertDataValueToObjectValue(Object o, Session sn) {
-    return Byte.MIN_VALUE;
-  }
-
-  @Override
-  public boolean isMutable() {
-    return false;
-  }
-
-  @Override
-  public void initialize(DatabaseMapping dm, Session sn) {
-    //Do nothing  
-  }
+//  @Override
+//  public Object convertObjectValueToDataValue(Object o, Session sn) {
+//    return Byte.MIN_VALUE;
+//  }
+//
+//  @Override
+//  public Object convertDataValueToObjectValue(Object o, Session sn) {
+//    return Byte.MIN_VALUE;
+//  }
+//
+//  @Override
+//  public boolean isMutable() {
+//    return false;
+//  }
+//
+//  @Override
+//  public void initialize(DatabaseMapping dm, Session sn) {
+//    //Do nothing  
+//  }
 
 }
