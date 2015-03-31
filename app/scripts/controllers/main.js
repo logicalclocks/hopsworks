f7f1547f0402ae62da7b56d1aebab44dd8d37983
More <amore@kth.se>
2015-03-31 20:40:23 +0200

    Navigation is now only showed when a user is logged in

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,32 +1,34 @@
 'use strict';
 
 angular.module('hopsWorksApp')
-  .controller('MainCtrl', ['$scope','$location', 'AuthService', function ($scope, $location, AuthService) {
+    .controller('MainCtrl', ['$scope', '$location', 'AuthService', function ($scope, $location, AuthService) {
 
-    var self = this;
-    self.authService = AuthService;
+        var self = this;
+        self.authService = AuthService;
 
-    // Check if the user is logged in when the application loads
-    // User Service will automatically update isLoggedIn after this call finishes
-    AuthService.session();
-    //self.userService.logout();
+        self.isLoggedIn = AuthService.isLoggedIn;
 
-
-    self.logout = function () {
-      console.log(self.user);
-      AuthService.logout(self.user).then(function (success) {
-        $location.url('/login');
-      }, function (error) {
-        self.errorMessage = error.data.msg;
-      });
-    };
+        // Check if the user is logged in when the application loads
+        // User Service will automatically update isLoggedIn after this call finishes
+        AuthService.session();
+        //self.userService.logout();
 
 
-    $scope.projects = [
-      {name: 'HumanGenome', 'private': true},
-      {name: 'DNACalc', 'private': false},
-      {name: 'FinanceDepartment', 'private': true},
-      {name: 'StatisticsHops', 'private': false}
-    ];
+        self.logout = function () {
+            console.log(self.user);
+            AuthService.logout(self.user).then(function (success) {
+                $location.url('/login');
+            }, function (error) {
+                self.errorMessage = error.data.msg;
+            });
+        };
 
-  }]);
+
+        $scope.projects = [
+            {name: 'HumanGenome', 'private': true},
+            {name: 'DNACalc', 'private': false},
+            {name: 'FinanceDepartment', 'private': true},
+            {name: 'StatisticsHops', 'private': false}
+        ];
+
+    }]);
