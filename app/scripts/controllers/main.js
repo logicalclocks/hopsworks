9fdce44a346f6a3cca6f4a7b4431f709c0b523eb
More <amore@kth.se>
2015-04-09 16:34:13 +0200

    Created ngResource for project services

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,28 +1,64 @@
 'use strict';
 
 angular.module('hopsWorksApp')
-    .controller('MainCtrl', ['$scope', '$location', 'AuthService', function ($scope, $location, AuthService) {
+  .controller('MainCtrl', ['$scope', '$location', 'AuthService', 'ProjectService', function ($scope, $location, AuthService, ProjectService) {
 
-        var self = this;
-        self.authService = AuthService;
+    var self = this;
 
-        self.isLoggedIn = AuthService.isLoggedIn;
+    self.authService = AuthService;
 
-        self.logout = function () {
-            console.log(self.user);
-            AuthService.logout(self.user).then(function (success) {
-                $location.url('/login');
-            }, function (error) {
-                self.errorMessage = error.data.msg;
-            });
-        };
+    self.isLoggedIn = AuthService.isLoggedIn;
+
+    self.logout = function () {
+      console.log(self.user);
+      AuthService.logout(self.user).then(function (success) {
+        $location.url('/login');
+      }, function (error) {
+        self.errorMessage = error.data.msg;
+      });
+    };
+
+    // Load all projects
+    self.projects = ProjectService.query();
 
 
-        $scope.projects = [
-            {name: 'HumanGenome', 'private': true},
-            {name: 'DNACalc', 'private': false},
-            {name: 'FinanceDepartment', 'private': true},
-            {name: 'StatisticsHops', 'private': false}
-        ];
 
-    }]);
+
+
+
+    /*******************************/
+    /* TESTING ALL CRUD OPERATIONS */
+    /*******************************/
+
+    // GET /api/project/
+    // $scope.projects = ProjectService.query();
+    // console.log($scope.projects);
+
+    // GET /api/project/1
+    // $scope.specificProject = ProjectService.get({}, {'id': 1});
+
+    // PUT /api/project/1
+    // $scope.specificProject.description = 'TESTING TO CHANGE VALUE';
+    // ProjectService.update({ id:$scope.specificProject.id }, $scope.specificProject );
+
+    // POST /api/project/
+    /*
+    $scope.newProject = {
+      'description':'Created a new project',
+      'name':'TestProject',
+      'status':0,
+      'type':'Spark'
+    }
+    */
+
+
+    // POST /api/project/
+    // ProjectService.save($scope.newProject);
+
+    // DELETE /api/project/ THELATEST id
+    // ProjectService.delete({}, {'id': 35 });
+
+
+
+
+  }]);
--- app/scripts/services/ProjectService.js
+++ app/scripts/services/ProjectService.js
@@ -1,18 +1,26 @@
 'use strict';
 
 angular.module('hopsWorksApp')
-    .factory('ProjectService', ['$http',
-        function ($http) {
-            return {
-                read: function () {
-                    return $http.get('/api/project/read');
-                },
-                write: function () {
-                    return $http.get('/api/project/write');
-                },
-                doSomething: function () {
-                    return $http.get('/api/project/doSomething');
-                }
-            };
-        }]);
-
+  .factory("ProjectService", function ($resource) {
+    return $resource(
+      "/api/project/:id",
+      {id: "@id"},
+      {
+        "save": {
+          method:"POST",
+          headers:{'Content-Type':'application/json; charset=UTF-8'}
+        },
+        "delete": {
+          method:"DELETE"
+        },
+        "update": {
+          method: "PUT",
+          headers:{'Content-Type':'application/json; charset=UTF-8'}
+        },
+        "projects": {
+          'method': 'GET',
+          isArray: true
+        }
+      }
+    );
+  });
