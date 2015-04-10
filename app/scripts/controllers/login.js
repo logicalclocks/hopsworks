4a24a2497736da762409836d85413723db4f4196
More <amore@kth.se>
2015-04-10 20:55:16 +0200

    Fixes #28, Added a cookie to be able to save the users email, and also keep containing it after a page refresh.

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,16 +1,18 @@
 'use strict';
 
 angular.module('hopsWorksApp')
-  .controller('LoginCtrl', ['$location', 'AuthService',
-    function ($location, AuthService) {
+  .controller('LoginCtrl', ['$location', '$cookieStore', 'AuthService',
+    function ($location, $cookieStore, AuthService) {
 
       var self = this;
+
       self.user = {email: '', password: ''};
 
       self.login = function () {
         AuthService.login(self.user).then(
           function (success) {
             $location.path('/');
+            $cookieStore.put('email', self.user.email);
         }, function (error) {
             self.errorMessage = error.data.msg;
         });
