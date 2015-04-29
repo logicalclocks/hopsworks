9d50a54895b197b3bf166b28ac8df2b97cb125ab
More <amore@kth.se>
2015-04-29 19:20:23 +0200

    Fixes #10, had problem with creating a new user due to exception that wasnt handled in the facade.

--- src/main/java/se/kth/hopsworks/users/UserFacade.java
+++ src/main/java/se/kth/hopsworks/users/UserFacade.java
@@ -9,7 +9,6 @@ import javax.persistence.TypedQuery;
 import se.kth.hopsworks.user.model.Users;
 import se.kth.kthfsdashboard.user.AbstractFacade;
 
-
 /**
  *
  * @author Jim Dowling<jdowling@sics.se>
@@ -17,99 +16,104 @@ import se.kth.kthfsdashboard.user.AbstractFacade;
  */
 @Stateless
 public class UserFacade extends AbstractFacade<Users> {
-    
-    private final int STARTING_USER = 1000;
 
-    @PersistenceContext(unitName = "kthfsPU")
-    private EntityManager em;
+  private final int STARTING_USER = 1000;
 
-    @Override
-    protected EntityManager getEntityManager() {
-        return em;
+  @PersistenceContext(unitName = "kthfsPU")
+  private EntityManager em;
+
+  @Override
+  protected EntityManager getEntityManager() {
+    return em;
+  }
+
+  public UserFacade() {
+    super(Users.class);
+  }
+
+  @Override
+  public List<Users> findAll() {
+    TypedQuery<Users> query = em.createNamedQuery("Users.findAll",
+            Users.class);
+    return query.getResultList();
+  }
+
+  public List<Users> findAllByName() {
+    TypedQuery<Users> query = em.createNamedQuery("Users.findAllByName",
+            Users.class);
+    return query.getResultList();
+  }
+
+  public List<Users> findAllUsers() {
+    Query query = em.createNativeQuery("SELECT * FROM USERS", Users.class);
+    return query.getResultList();
+  }
+
+  public List<Users> filterUsersBasedOnStudy(String name) {
+
+    Query query = em.createNativeQuery(
+            "SELECT * FROM USERS WHERE email NOT IN (SELECT team_member FROM StudyTeam WHERE name=?)",
+            Users.class).setParameter(1, name);
+    return query.getResultList();
+  }
+
+  public void persist(Users user) {
+    em.persist(user);
+  }
+
+  public int lastUserID() {
+    Query query = em.createNativeQuery("SELECT MAX(p.uid) FROM users p");
+    Object obj = query.getSingleResult();
+
+    if (obj == null) {
+      return STARTING_USER;
     }
+    return (Integer) obj;
+  }
 
-    public UserFacade() {
-        super(Users.class);
+  public void update(Users user) {
+    em.merge(user);
+  }
+
+  public void removeByEmail(String email) {
+    Users user = findByEmail(email);
+    if (user != null) {
+      em.remove(user);
     }
+  }
 
-    @Override
-    public List<Users> findAll() {
-        TypedQuery<Users> query = em.createNamedQuery("Users.findAll",
-                Users.class);
-        return query.getResultList();
+  @Override
+  public void remove(Users user) {
+    if (user != null && user.getEmail() != null && em.contains(user)) {
+      em.remove(user);
     }
+  }
 
-    public List<Users> findAllByName() {
-        TypedQuery<Users> query = em.createNamedQuery("Users.findAllByName",
-                Users.class);
-        return query.getResultList();
+  public Users findByEmail(String email) {
+    try {
+      return em.createNamedQuery("Users.findByEmail", Users.class).setParameter(
+              "email", email)
+              .getSingleResult();
+    } catch (Exception e) {
+      return null;
     }
+  }
 
-    public List<Users> findAllUsers() {
-        Query query = em.createNativeQuery("SELECT * FROM USERS", Users.class);
-        return query.getResultList();
-    }
+  public void detach(Users user) {
+    em.detach(user);
+  }
 
-    public List<Users> filterUsersBasedOnStudy(String name) {
-
-        Query query = em.createNativeQuery(
-                "SELECT * FROM USERS WHERE email NOT IN (SELECT team_member FROM StudyTeam WHERE name=?)",
-                Users.class).setParameter(1, name);
-        return query.getResultList();
-    }
-
-    public void persist(Users user) {
-        em.persist(user);
-    }
-
-    public int lastUserID() {
-        Query query = em.createNativeQuery("SELECT MAX(p.uid) FROM users p");
-        Object obj = query.getSingleResult();
-
-        if (obj == null) {
-            return STARTING_USER;
-        }
-        return (Integer) obj;
-    }
-
-    public void update(Users user) {
-        em.merge(user);
-    }
-
-    public void removeByEmail(String email) {
-        Users user = findByEmail(email);
-        if (user != null) {
-            em.remove(user);
-        }
-    }
-
-    @Override
-    public void remove(Users user) {
-        if (user != null && user.getEmail() != null && em.contains(user)) {
-            em.remove(user);
-        }
-    }
-
-    public Users findByEmail(String email) {
-        return em.createNamedQuery("Users.findByEmail", Users.class).setParameter("email", email)
-                .getSingleResult();
-    }
-
-    public void detach(Users user) {
-        em.detach(user);
-    }
-
-    /**
-     * Get all users with STATUS = status.
-     *
-     * @param status
-     * @return
-     */
-    public List<Users> findAllByStatus(int status) {
-        TypedQuery<Users> query = em.createNamedQuery("Users.findByStatus",
-                Users.class);
-        query.setParameter("status", status);
-        return query.getResultList();
-    }
+  /**
+   * Get all users with STATUS = status.
+   *
+   * @param status
+   * @return
+   */
+  public List<Users> findAllByStatus(int status) {
+    TypedQuery<Users> query = em.createNamedQuery("Users.findByStatus",
+            Users.class);
+    query.setParameter("status", status);
+    return query.getResultList();
+  }
 
 }
