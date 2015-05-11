0bf15b48982d2a19e4477f9be18c3d254672fc5f
Vangelis <vsavidis@gmail.com>
2015-05-11 15:37:03 +0200

    garbage removed

--- hopsWorksFE/public_html/app/scripts/controllers/MainController.js
+++ hopsWorksFE/public_html/app/scripts/controllers/MainController.js
@@ -106,18 +106,6 @@ var app = angular.module('metaUI')
                                     });
                         };
 
-$scope.person = {};
-  $scope.people = [
-    { name: 'Adam',      email: 'adam@email.com',      age: 10 },
-    { name: 'Amalie',    email: 'amalie@email.com',    age: 12 },
-    { name: 'Wladimir',  email: 'wladimir@email.com',  age: 30 },
-    { name: 'Samantha',  email: 'samantha@email.com',  age: 31 },
-    { name: 'Estefanía', email: 'estefanía@email.com', age: 16 },
-    { name: 'Natasha',   email: 'natasha@email.com',   age: 54 },
-    { name: 'Nicole',    email: 'nicole@email.com',    age: 43 },
-    { name: 'Adrian',    email: 'adrian@email.com',    age: 21 }
-  ];
-
                         $scope.$on("documentClicked", function (event, data) {
                             _close();
                         });
