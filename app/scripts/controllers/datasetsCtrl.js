25d89d2bd1a45cb7a65cd624dbdc3c1f1b6dfd08
More <amore@kth.se>
2015-05-15 22:00:31 +0200

    Part 2

--- app/scripts/controllers/datasetsCtrl.js
+++ app/scripts/controllers/datasetsCtrl.js
@@ -6,8 +6,8 @@
 
 
 angular.module('hopsWorksApp')
-  .controller('DatasetsCtrl', ['$scope', '$timeout', '$mdSidenav', '$mdUtil', '$log', '$websocket', 'WSComm',
-    function ($scope, $timeout, $mdSidenav, $mdUtil, $log, $websocket, WSComm) {
+  .controller('DatasetsCtrl', ['$rootScope', '$modal', '$scope', '$timeout', '$mdSidenav', '$mdUtil', '$log', '$websocket', 'WSComm',
+    function ($rootScope, $modal, $scope, $timeout, $mdSidenav, $mdUtil, $log, $websocket, WSComm) {
 
       var self = this;
 
@@ -70,6 +70,9 @@ angular.module('hopsWorksApp')
           });
       };
 
+
+
+
       self.availableTemplates = [];
 
       self.newTemplateName = "";
@@ -77,7 +80,12 @@ angular.module('hopsWorksApp')
 
       self.extendedFromBoard = {};
 
-      self.currentTemplate = {};
+      self.currentTemplateID = "";
+      self.currentBoard = {};
+
+
+
+
 
       self.getAllTemplates = function () {
         WSComm.send({
@@ -88,7 +96,6 @@ angular.module('hopsWorksApp')
         }).then(
           function (data) {
             self.availableTemplates = JSON.parse(data.board).templates;
-            console.log(self.availableTemplates);
           }
         );
       }
@@ -125,6 +132,35 @@ angular.module('hopsWorksApp')
 
 
 
+      $scope.$watch('extendedFrom', function(newID){
+        if (typeof newID == "string"){
+          self.selectChanged(newID);
+        }
+      });
+
+      self.selectChanged = function(extendFromThisID){
+        console.log('selectChanged - start: ' + extendFromThisID);
+        return WSComm.send({
+          sender: 'evsav',
+          type: 'TemplateMessage',
+          action: 'fetch_template',
+          message: JSON.stringify({tempid: parseInt(extendFromThisID)})
+        }).then(
+          function(success){
+            console.log('Fetched data - success.board.column:');
+            self.extendedFromBoard = JSON.parse(success.board);
+            console.log(self.extendedFromBoard);
+
+            console.log('Fetched data - success:');
+            console.log(success);
+
+          }, function(error){
+            console.log('Fetched data - error:');
+            console.log(error);
+          }
+        )
+      }
+
       self.extendTemplate = function(){
         return WSComm.send({
           sender: 'evsav',
@@ -135,6 +171,11 @@ angular.module('hopsWorksApp')
           function(data){
             var tempTemplates = JSON.parse(data.board);
             var newlyCreatedID = tempTemplates.templates[tempTemplates.numberOfTemplates-1].id;
+            console.log('add_new_templatE');
+            console.log(data);
+
+            console.log('Sent message: ');
+            console.log(self.extendedFromBoard);
 
             return WSComm.send({
               sender: 'evsav',
@@ -156,28 +197,185 @@ angular.module('hopsWorksApp')
       }
 
 
-      $scope.$watch('extendedFrom', function(newID){
-        if (typeof newID == "string"){
-          self.selectChanged(newID);
-        }
-      });
-
-      self.selectChanged = function(extendFromThisID){
-        console.log('selectChanged - start: ' + extendFromThisID);
+      self.fetchTemplate = function (templateId) {
+        self.currentTemplateID = templateId;
         return WSComm.send({
           sender: 'evsav',
           type: 'TemplateMessage',
           action: 'fetch_template',
-          message: JSON.stringify({tempid: parseInt(extendFromThisID)})
+          message: JSON.stringify({tempid: templateId})
         }).then(
-          function(data){
-            self.extendedFromBoard = data.board.columns;
-            console.log(data);
+          function(success){
+            console.log('fetchTemplate - success');
+            self.currentBoard = JSON.parse(success.board);
+            console.log(self.currentBoard);
+          }, function(error){
+            console.log('fetchTemplate - error');
+            console.log(JSON.parse(error));
+          }
+        );
+      };
+
+
+      self.storeTemplate = function () {
+        return WSComm.send({
+          sender: 'evsav',
+          type: 'TemplateMessage',
+          action: 'store_template',
+          message: JSON.stringify({tempid: self.currentTemplateID, bd: self.currentBoard})
+        }).then(
+          function(success){
+            console.log(success);
           }, function(error){
             console.log(error);
           }
-        )
+        );
       }
+
+
+
+
+      self.deleteList = function (column) {
+        return WSComm.send({
+          sender: 'evsav',
+          type: 'TablesMessage',
+          action: 'delete_table',
+          message: JSON.stringify({
+            tempid: self.currentTemplateID,
+            id: column.id,
+            name: column.name,
+            forceDelete: column.forceDelete
+          })
+        }).then(
+          function(success){
+            console.log(success);
+            self.fetchTemplate(self.currentTemplateID)
+          }, function(error){
+            console.log(error);
+          }
+        );
+      }
+
+
+      self.storeCard = function (column, card) {
+        return WSComm.send({
+          sender: 'evsav',
+          type: 'FieldsMessage',
+          action: 'store_field',
+          message: JSON.stringify({
+            tempid: self.currentTemplateID,
+            tableid: column.id,
+            tablename: column.name,
+            id: card.id,
+            name: card.title,
+            type: 'VARCHAR(50)',
+            searchable: card.find,
+            required: card.required,
+            sizefield: card.sizefield,
+            description: card.description,
+            fieldtypeid: card.fieldtypeid,
+            fieldtypeContent: card.fieldtypeContent
+          })
+        })
+      }
+
+
+
+
+      self.addCard = function (column) {
+        $scope.currentColumn = column;
+
+        var modalInstance = $modal.open({
+            templateUrl: 'views/metadata/newCardModal.html',
+            controller: 'NewCardCtrl',
+            scope: $scope
+         }).result.then(
+          function(card){
+
+            console.log('Created card, ready to send:');
+            console.log(card);
+
+            self.storeCard(column, card).then(
+              function (success) {
+                console.log(success);
+                self.fetchTemplate(self.currentTemplateID)
+              }, function(error){
+                console.log(error);
+              }
+            );
+
+          }, function(error){
+            console.log(error);
+          }
+        );
+      };
+
+
+      self.deleteCard = function (column, card) {
+        return WSComm.send({
+          sender: 'evsav',
+          type: 'FieldsMessage',
+          action: 'delete_field',
+          message: JSON.stringify({
+            tempid: self.currentTemplateID,
+            id: card.id,
+            tableid: column.id,
+            tablename: column.name,
+            name: card.title,
+            type: 'VARCHAR(50)',
+            sizefield: card.sizefield,
+            searchable: card.find,
+            required: card.required,
+            forceDelete: card.forceDelete,
+            description: card.description,
+            fieldtypeid: card.fieldtypeid,
+            fieldtypeContent: card.fieldtypeContent
+          })
+        }).then(
+          function(success){
+            console.log(success);
+            self.fetchTemplate(self.currentTemplateID)
+        }, function(error){
+            console.log(error);
+        });
+      }
+
+
+
+
+
+
+      self.addNewList = function () {
+        $scope.template = self.currentTemplateID;
+
+
+        $modal.open({
+          templateUrl: 'views/metadata/newListModal.html',
+          controller: 'NewListCtrl',
+          scope: $scope
+        });
+
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+      /* TESTING RECEIVE BROADCAST FROM WEBSOCKET SERVICE */
+      $rootScope.$on('andreTesting', function (event, data) {
+        console.log('BroadcastReceived BOARD:');
+        console.log(JSON.parse(data.response.board));
+        //self.getAllTemplates();
+      });
+
 
 
 
@@ -204,32 +402,38 @@ angular.module('hopsWorksApp')
      },
      <DONE>
 
- fetchTemplate: function (templateId) {
- return WSComm.send({
- sender: 'evsav',
- type: 'TemplateMessage',
- action: 'fetch_template',
- message: JSON.stringify({tempid: templateId})
- });
- },
+   <DONE>
+     fetchTemplate: function (templateId) {
+     return WSComm.send({
+     sender: 'evsav',
+     type: 'TemplateMessage',
+     action: 'fetch_template',
+     message: JSON.stringify({tempid: templateId})
+     });
+     },
+   <DONE>
 
- storeTemplate: function (templateId, board) {
- return WSComm.send({
- sender: 'evsav',
- type: 'TemplateMessage',
- action: 'store_template',
- message: JSON.stringify({tempid: templateId, bd: board})
- });
- },
+  <DONE>
+     storeTemplate: function (templateId, board) {
+     return WSComm.send({
+     sender: 'evsav',
+     type: 'TemplateMessage',
+     action: 'store_template',
+     message: JSON.stringify({tempid: templateId, bd: board})
+     });
+     },
+  <DONE>
 
- extendTemplate: function(templateId, board){
- return WSComm.send({
- sender: 'evsav',
- type: 'TemplateMessage',
- action: 'extend_template',
- message: JSON.stringify({tempid: templateId, bd: board})
- });
- },
+    <DONE>
+     extendTemplate: function(templateId, board){
+     return WSComm.send({
+     sender: 'evsav',
+     type: 'TemplateMessage',
+     action: 'extend_template',
+     message: JSON.stringify({tempid: templateId, bd: board})
+     });
+     },
+      <DONE>
 
      <DONE>
      addNewTemplate: function(templateName){
@@ -253,65 +457,103 @@ angular.module('hopsWorksApp')
      },
      <DONE>
 
- deleteList: function (templateId, column) {
- return WSComm.send({
- sender: 'evsav',
- type: 'TablesMessage',
- action: 'delete_table',
- message: JSON.stringify({
- tempid: templateId,
- id: column.id,
- name: column.name,
- forceDelete: column.forceDelete
- })
- });
- },
+     deleteList: function (templateId, column) {
+     return WSComm.send({
+     sender: 'evsav',
+     type: 'TablesMessage',
+     action: 'delete_table',
+     message: JSON.stringify({
+     tempid: templateId,
+     id: column.id,
+     name: column.name,
+     forceDelete: column.forceDelete
+     })
+     });
+     },
 
- storeCard: function (templateId, column, card) {
- return WSComm.send({
- sender: 'evsav',
- type: 'FieldsMessage',
- action: 'store_field',
- message: JSON.stringify({
- tempid: templateId,
- tableid: column.id,
- tablename: column.name,
- id: card.id,
- name: card.title,
- type: 'VARCHAR(50)',
- searchable: card.find,
- required: card.required,
- sizefield: card.sizefield,
- description: card.description,
- fieldtypeid: card.fieldtypeid,
- fieldtypeContent: card.fieldtypeContent
- })
- });
- },
+     <DONE>
 
- deleteCard: function (templateId, column, card) {
 
- return WSComm.send({
- sender: 'evsav',
- type: 'FieldsMessage',
- action: 'delete_field',
- message: JSON.stringify({
- tempid: templateId,
- id: card.id,
- tableid: column.id,
- tablename: column.name,
- name: card.title,
- type: 'VARCHAR(50)',
- sizefield: card.sizefield,
- searchable: card.find,
- required: card.required,
- forceDelete: card.forceDelete,
- description: card.description,
- fieldtypeid: card.fieldtypeid,
- fieldtypeContent: card.fieldtypeContent
- })
- });
- },
+     <DONE>
+     self.storeCard = function (column) {
+     return WSComm.send({
+     sender: 'evsav',
+     type: 'FieldsMessage',
+     action: 'store_field',
+     message: JSON.stringify({
+     tempid: self.currentTemplateID,
+     tableid: column.id,
+     tablename: column.name,
+     id: card.id,
+     name: card.title,
+     type: 'VARCHAR(50)',
+     searchable: card.find,
+     required: card.required,
+     sizefield: card.sizefield,
+     description: card.description,
+     fieldtypeid: card.fieldtypeid,
+     fieldtypeContent: card.fieldtypeContent
+     })
+     }).then(
+     function(success){
+     console.log(success);
+     }, function(error){
+     console.log(error);
+     }
+     );
+     }
+     <DONE>
+
+      <DONE>
+     storeCard: function (templateId, column, card) {
+     return WSComm.send({
+     sender: 'evsav',
+     type: 'FieldsMessage',
+     action: 'store_field',
+     message: JSON.stringify({
+     tempid: templateId,
+     tableid: column.id,
+     tablename: column.name,
+     id: card.id,
+     name: card.title,
+     type: 'VARCHAR(50)',
+     searchable: card.find,
+     required: card.required,
+     sizefield: card.sizefield,
+     description: card.description,
+     fieldtypeid: card.fieldtypeid,
+     fieldtypeContent: card.fieldtypeContent
+     })
+     });
+     },
+     <DONE>
+
+     <DONE>
+     deleteCard: function (templateId, column, card) {
+
+     return WSComm.send({
+     sender: 'evsav',
+     type: 'FieldsMessage',
+     action: 'delete_field',
+     message: JSON.stringify({
+     tempid: templateId,
+     id: card.id,
+     tableid: column.id,
+     tablename: column.name,
+     name: card.title,
+     type: 'VARCHAR(50)',
+     sizefield: card.sizefield,
+     searchable: card.find,
+     required: card.required,
+     forceDelete: card.forceDelete,
+     description: card.description,
+     fieldtypeid: card.fieldtypeid,
+     fieldtypeContent: card.fieldtypeContent
+     })
+     });
+     },
+     <DONE>
+
 
  storeMetadata: function(data){
  return WSComm.send({
@@ -331,14 +573,17 @@ angular.module('hopsWorksApp')
  });
  },
 
- fetchFieldTypes: function(){
- return WSComm.send({
- sender: 'evsav',
- type: 'FieldTypesMessage',
- action: 'fetch_field_types',
- message: 'null'
- });
- }
+   <DONE>
+
+   fetchFieldTypes: function(){
+       return WSComm.send({
+       sender: 'evsav',
+       type: 'FieldTypesMessage',
+       action: 'fetch_field_types',
+       message: 'null'
+       });
+       }
+   <DONE>
 
 
  */
