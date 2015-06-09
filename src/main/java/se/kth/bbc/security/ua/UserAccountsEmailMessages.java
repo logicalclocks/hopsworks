2387d770ab78b4ec6aab2aeaf45c62ce80152471
Stig Viaene <stigviaene@sics.se>
2015-06-09 13:57:40 +0200

    Removed license header template

--- src/main/java/se/kth/bbc/security/ua/UserAccountsEmailMessages.java
+++ src/main/java/se/kth/bbc/security/ua/UserAccountsEmailMessages.java
@@ -1,13 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package se.kth.bbc.security.ua;
-
-import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.swing.text.AbstractDocument;
 
 /**
  * This class contain the email messages that are sent to the users for account
--- src/main/java/se/kth/bbc/upload/ResumableInfoStorage.java
+++ src/main/java/se/kth/bbc/upload/ResumableInfoStorage.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package se.kth.bbc.upload;
 
-import java.io.*;
 import java.util.HashMap;
 
 /**
