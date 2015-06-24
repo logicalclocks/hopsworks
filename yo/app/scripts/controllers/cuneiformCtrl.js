bb8205b6dd3b82c565269ae76eefcbf5621eadfc
Stig Viaene <stigviaene@sics.se>
2015-06-24 19:12:23 +0200

    Implemented displaying currently selected job and updating of job history.

--- yo/app/scripts/controllers/cuneiformCtrl.js
+++ yo/app/scripts/controllers/cuneiformCtrl.js
@@ -76,8 +76,11 @@ angular.module('hopsWorksApp')
             self.execute = function () {
               CuneiformService.runWorkflow(self.pId, self.workflow).then(
                       function (success) {
-                        //Do something
+                        //get the resulting job
                         self.job = success.data;
+                        //Add it to the history array
+                        self.jobs.unshift(self.job);
+                        //Reset the configuration data
                         self.workflow = null;
                         //Start polling
                         poller = $interval(pollStatus, 3000);
@@ -89,26 +92,47 @@ angular.module('hopsWorksApp')
             var pollStatus = function () {
               JobHistoryService.pollStatus(self.pId, self.job.id).then(
                       function (success) {
+                        var oldindex = self.jobs.indexOf(self.job);
                         self.job = success.data;
+                        //Replace the old element in the jobs array
+                        if (oldindex !== -1) {
+                          self.jobs[oldindex] = self.job;
+                        }
                         //check if job finished
-                        if (self.job.state == 'FINISHED' 
-                                || self.job.state == 'FAILED' 
-                                || self.job.state == 'KILLED' 
-                                || self.job.state == 'FRAMEWORK_FAILURE' 
-                                || self.job.state == 'APP_MASTER_START_FAILED'){
+                        if (self.job.state == 'FINISHED'
+                                || self.job.state == 'FAILED'
+                                || self.job.state == 'KILLED'
+                                || self.job.state == 'FRAMEWORK_FAILURE'
+                                || self.job.state == 'APP_MASTER_START_FAILED') {
                           //if so: stop executing
                           $interval.cancel(poller);
                         }
                       }, function (error) {
-                         $interval.cancel(poller);
-                         //TODO: display error message
+                $interval.cancel(poller);
+                //TODO: display error message
               })
             };
             var poller;
-            
-            $scope.$on('$destroy',function(){
+
+            $scope.$on('$destroy', function () {
               $interval.cancel(poller);
             });
+
+            self.selectJob = function (job) {
+              //Stop polling
+              $interval.cancel(poller);
+              //Set self.job
+              self.job = job;
+              //check if need to start polling
+              if (self.job.state !== 'FINISHED'
+                      && self.job.state !== 'FAILED'
+                      && self.job.state !== 'KILLED'
+                      && self.job.state !== 'FRAMEWORK_FAILURE'
+                      && self.job.state !== 'APP_MASTER_START_FAILED') {
+                //if so: start executing
+                poller = $interval(pollStatus,3000);
+              }
+            };
 
           }]);
 
