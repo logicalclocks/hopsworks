2c0d78a6ff9f71dabec1d217772c7a1383ff85e4
Stig Viaene <stigviaene@sics.se>
2015-06-30 16:32:08 +0200

    Cleaned up js

--- yo/app/scripts/controllers/cuneiformCtrl.js
+++ yo/app/scripts/controllers/cuneiformCtrl.js
@@ -5,7 +5,6 @@
 
 'use strict';
 
-
 angular.module('hopsWorksApp')
         .controller('CuneiformCtrl', ['$scope', '$timeout', '$mdSidenav', '$mdUtil', '$log', '$location', '$routeParams', 'growl', 'ModalService', 'JobHistoryService', 'CuneiformService', '$interval',
           function ($scope, $timeout, $mdSidenav, $mdUtil, $log, $location, $routeParams, growl, ModalService, JobHistoryService, CuneiformService, $interval) {
@@ -13,31 +12,6 @@ angular.module('hopsWorksApp')
             var self = this;
 
             self.pId = $routeParams.projectID;
-
-            self.toggleLeft = buildToggler('left');
-            self.toggleRight = buildToggler('right');
-            /**
-             * Build handler to open/close a SideNav; when animation finishes
-             * report completion in console
-             */
-            function buildToggler(navID) {
-              var debounceFn = $mdUtil.debounce(function () {
-                $mdSidenav(navID)
-                        .toggle()
-                        .then(function () {
-                          $log.debug("toggle " + navID + " is done");
-                        });
-              }, 300);
-              return debounceFn;
-            }
-            ;
-
-            self.close = function () {
-              $mdSidenav('right').close()
-                      .then(function () {
-                        $log.debug("close RIGHT is done");
-                      });
-            };
 
             /*
              * Get all Cuneiform job history objects for this project.
