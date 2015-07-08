0e0d67acc34a20f529c53e2f0976d7ada164b21c
Stig Viaene <stigviaene@sics.se>
2015-07-08 14:21:16 +0200

    Added backend support for Spark settings.

--- src/main/java/se/kth/hopsworks/controller/SparkController.java
+++ src/main/java/se/kth/hopsworks/controller/SparkController.java
@@ -72,6 +72,15 @@ public class SparkController {
     runnerbuilder.setJobName(jobconfig.getAppName());
     String[] jobArgs = jobconfig.getArgs().trim().split(" ");
     runnerbuilder.addAllJobArgs(jobArgs);
+    //Set spark runner options
+    runnerbuilder.setExecutorCores(jobconfig.getExecutorCores());
+    runnerbuilder.setExecutorMemory(jobconfig.getExecutorMemory());
+    runnerbuilder.setNumberOfExecutors(jobconfig.getNumberOfExecutors());
+    //Set Yarn running options
+    runnerbuilder.setDriverMemoryMB(jobconfig.getAmMemory());
+    runnerbuilder.setDriverCores(jobconfig.getAmVCores());
+    runnerbuilder.setDriverQueue(jobconfig.getAmQueue());
+    
     //TODO: runnerbuilder.setExtraFiles(config.getExtraFiles());
 
     YarnRunner r;
