9568e27dcb5cbe9e95f9cd627f3104f0db9b516b
Stig Viaene <stigviaene@sics.se>
2015-07-28 15:36:40 +0200

    Updated job details GUI

--- yo/app/scripts/controllers/jobDetailCtrl.js
+++ yo/app/scripts/controllers/jobDetailCtrl.js
@@ -8,11 +8,15 @@ angular.module('hopsWorksApp')
 
             var self = this;
             this.job = job;
+            this.jobtype; //Holds the type of job.
+            this.execFile; //Holds the name of the main execution file
+            this.showExecutions = false;
 
             var getConfiguration = function () {
               JobService.getConfiguration(projectId, job.id).then(
                       function (success) {
                         self.job.runConfig = success.data;
+                        self.setupInfo();
                       }, function (error) {
                 growl.error(error.data.errorMsg, {title: 'Error fetching job configuration.', ttl: 15000});
               });
@@ -22,11 +26,25 @@ angular.module('hopsWorksApp')
               JobService.getAllExecutions(projectId, job.id).then(
                       function (success) {
                         self.job.executions = success.data;
+                        self.showExecutions = success.data.length > 0;
                       }, function (error) {
                 growl.error(error.data.errorMsg, {title: 'Error fetching execution history.', ttl: 15000});
               });
             };
-            
+
+            this.setupInfo = function () {
+              if (self.job.runConfig.type == "cuneiformJobConfiguration") {
+                self.jobtype = "Cuneiform";
+                self.execFile = job.runConfig.wf.name;
+              } else if (self.job.runConfig.type == "sparkJobConfiguration") {
+                self.jobtype = "Spark";
+                self.execFile = getFileName(job.runConfig.jarPath);
+              } else if (self.job.runConfig.type == "adamJobConfiguration") {
+                self.jobtype = "ADAM";
+                self.execFile = job.runConfig.selectedCommand.command;
+              }
+            };
+
             getConfiguration();
             getExecutions();
 
