35b98fe5be37bf3ccfa31779db1a69ad105c112f
Vangelis <vsavidis@gmail.com>
2015-08-05 17:53:09 +0200

    added missing foreign key constraint to entities

--- src/main/java/se/kth/meta/entity/RawData.java
+++ src/main/java/se/kth/meta/entity/RawData.java
@@ -69,8 +69,10 @@ public class RawData implements Serializable, EntityIntf {
   @Column(name = "data")
   private String data;
 
-  @Transient
-  private int inodeid;
+  @ManyToOne(optional = false)
+  @PrimaryKeyJoinColumn(name = "tupleid",
+          referencedColumnName = "tupleid")
+  private TupleToFile tupleToFile;
 
   public RawData() {
   }
@@ -116,11 +118,11 @@ public class RawData implements Serializable, EntityIntf {
   /*
    * get and set the parent entity
    */
-  public void setFields(Field fields) {
+  public void setField(Field fields) {
     this.fields = fields;
   }
 
-  public Field getFields() {
+  public Field getField() {
     return this.fields;
   }
   /*
@@ -135,20 +137,23 @@ public class RawData implements Serializable, EntityIntf {
     this.tupleid = tupleid;
   }
 
+  /*
+   * get and set the parent entity
+   */
+  public void setTupleToFile(TupleToFile ttf) {
+    this.tupleToFile = ttf;
+  }
+
+  public TupleToFile getTupleToFile() {
+    return this.tupleToFile;
+  }
+
   public String getData() {
     return data;
   }
 
   public void setData(String data) {
     this.data = data;
-  }
-
-  public int getInodeid() {
-    return this.inodeid;
-  }
-
-  public void setInodeid(int inodeid) {
-    this.inodeid = inodeid;
   }
 
   @Override
