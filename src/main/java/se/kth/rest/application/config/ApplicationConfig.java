c174e9c754dffdb7771d2dc8d6058439cd80e4c8
Vangelis <vsavidis@gmail.com>
2015-08-11 16:47:52 +0200

    disabled hdfs rest services to prevent them from messing with hopsworks' ones

--- src/main/java/se/kth/rest/application/config/ApplicationConfig.java
+++ src/main/java/se/kth/rest/application/config/ApplicationConfig.java
@@ -1,51 +1,38 @@
 package se.kth.rest.application.config;
 
-import java.util.Set;
-import javax.ws.rs.core.Application;
-import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
 
 /**
  *
  * @author Ermias
  */
 @javax.ws.rs.ApplicationPath("api")
-public class ApplicationConfig extends Application {
+public class ApplicationConfig extends ResourceConfig {
 
-  @Override
-  public Set<Class<?>> getClasses() {
-    Set<Class<?>> resources = new java.util.HashSet<>();
-    resources.add(MultiPartFeature.class);
-    addRestResourceClasses(resources);
-    return resources;
-  }
+  public ApplicationConfig() {
+    packages("se.kth.hopsworks.filters;se.kth.hopsworks.rest");
 
-  /**
-   * Do not modify addRestResourceClasses() method.
-   * It is automatically populated with
-   * all resources defined in the project.
-   * If required, comment out calling this method in getClasses().
-   */
-  private void addRestResourceClasses(Set<Class<?>> resources) {
-    resources.add(se.kth.hopsworks.filters.RequestAuthFilter.class);
-    resources.add(se.kth.hopsworks.rest.ActivityService.class);
-    resources.add(se.kth.hopsworks.rest.AdamService.class);
-    resources.add(se.kth.hopsworks.rest.AppExceptionMapper.class);
-    resources.add(se.kth.hopsworks.rest.AuthExceptionMapper.class);
-    resources.add(se.kth.hopsworks.rest.AuthService.class);
-    resources.add(se.kth.hopsworks.rest.CuneiformService.class);
-    resources.add(se.kth.hopsworks.rest.DataSetService.class);
-    resources.add(se.kth.hopsworks.rest.ExecutionService.class);
-    resources.add(se.kth.hopsworks.rest.JobService.class);
-    resources.add(se.kth.hopsworks.rest.ProjectMembers.class);
-    resources.add(se.kth.hopsworks.rest.ProjectService.class);
-    resources.add(se.kth.hopsworks.rest.RequestService.class);
-    resources.add(se.kth.hopsworks.rest.SparkService.class);
-    resources.add(se.kth.hopsworks.rest.ThrowableExceptionMapper.class);
-    resources.add(se.kth.hopsworks.rest.TransactionExceptionMapper.class);
-    resources.add(se.kth.hopsworks.rest.UploadService.class);
-    resources.add(se.kth.hopsworks.rest.UserService.class);
-    resources.add(se.kth.hopsworks.zeppelin.rest.InterpreterRestApi.class);
-    resources.add(se.kth.hopsworks.zeppelin.rest.NotebookRestApi.class);
-    resources.add(se.kth.hopsworks.zeppelin.rest.ZeppelinRestApi.class);
+    register(se.kth.hopsworks.filters.RequestAuthFilter.class);
+    register(se.kth.hopsworks.rest.ActivityService.class);
+    register(se.kth.hopsworks.rest.AdamService.class);
+    register(se.kth.hopsworks.rest.AppExceptionMapper.class);
+    register(se.kth.hopsworks.rest.AuthExceptionMapper.class);
+    register(se.kth.hopsworks.rest.AuthService.class);
+    register(se.kth.hopsworks.rest.CuneiformService.class);
+    register(se.kth.hopsworks.rest.DataSetService.class);
+    register(se.kth.hopsworks.rest.ExecutionService.class);
+    register(se.kth.hopsworks.rest.JobService.class);
+    register(se.kth.hopsworks.rest.ProjectMembers.class);
+    register(se.kth.hopsworks.rest.ProjectService.class);
+    register(se.kth.hopsworks.rest.RequestService.class);
+    register(se.kth.hopsworks.rest.SparkService.class);
+    register(se.kth.hopsworks.rest.ThrowableExceptionMapper.class);
+    register(se.kth.hopsworks.rest.TransactionExceptionMapper.class);
+    register(se.kth.hopsworks.rest.UploadService.class);
+    register(se.kth.hopsworks.rest.UserService.class);
+
+    // Enable Tracing support.
+    property(ServerProperties.TRACING, "ALL");
   }
 }
