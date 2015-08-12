1e7cb48492561f5d5b9e352dc0d7cdbb86c1649b
Vangelis <vsavidis@gmail.com>
2015-08-12 16:14:24 +0200

    added all services in applicationConfig

--- src/main/java/se/kth/rest/application/config/ApplicationConfig.java
+++ src/main/java/se/kth/rest/application/config/ApplicationConfig.java
@@ -13,6 +13,12 @@ public class ApplicationConfig extends ResourceConfig {
    * adding manually all the restful services of the application.
    */
   public ApplicationConfig() {
+    register(com.fasterxml.jackson.jaxrs.base.JsonMappingExceptionMapper.class);
+    register(com.fasterxml.jackson.jaxrs.base.JsonParseExceptionMapper.class);
+    register(com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider.class);
+    register(com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider.class);
+    register(com.fasterxml.jackson.jaxrs.json.JsonMappingExceptionMapper.class);
+    register(com.fasterxml.jackson.jaxrs.json.JsonParseExceptionMapper.class);
     register(se.kth.hopsworks.filters.RequestAuthFilter.class);
     register(se.kth.hopsworks.rest.ActivityService.class);
     register(se.kth.hopsworks.rest.AdamService.class);
@@ -31,5 +37,8 @@ public class ApplicationConfig extends ResourceConfig {
     register(se.kth.hopsworks.rest.TransactionExceptionMapper.class);
     register(se.kth.hopsworks.rest.UploadService.class);
     register(se.kth.hopsworks.rest.UserService.class);
+    register(se.kth.hopsworks.zeppelin.rest.InterpreterRestApi.class);
+    register(se.kth.hopsworks.zeppelin.rest.NotebookRestApi.class);
+    register(se.kth.hopsworks.zeppelin.rest.ZeppelinRestApi.class);
   }
 }
