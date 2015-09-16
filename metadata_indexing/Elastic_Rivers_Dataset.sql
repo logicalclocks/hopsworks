8ffb166f189fd22f085bfb311c3701d247659df9
Vangelis <vsavidis@gmail.com>
2015-09-16 14:38:50 +0200

    selective insert of dataset records (added-deleted) into buffer table

--- metadata_indexing/Elastic_Rivers_Dataset.sql
+++ metadata_indexing/Elastic_Rivers_Dataset.sql
@@ -1,23 +1,37 @@
 -- TAKE A BATCH OF RECORDS INTO THE BUFFER TABLE
 
 INSERT INTO hopsworks.meta_inodes_ops_datasets_deleted (inodeid, parentid, processed) 
-(SELECT hops.hdfs_metadata_log.inode_id, hops.hdfs_metadata_log.dataset_id, 0 FROM hops.hdfs_metadata_log LIMIT 100)
+(SELECT hops.hdfs_metadata_log.inode_id, hops.hdfs_metadata_log.dataset_id, 0 FROM hops.hdfs_metadata_log LIMIT 100);
 
--- SELECTIVELY DUMP TAKE A BATCH OF DATASET RECORDS INTO THE BUFFER TABLE (convenient when it comes to update the buffer table)
+-- SELECTIVELY DUMP A BATCH OF DATASET RECORDS INTO THE BUFFER TABLE - A TWO STEP PROCESS
+
+-- FIRST TAKE ADDED RECORDS (OPERATION = 0 - implicit since added records reside in hdfs_inodes table)
 
 INSERT INTO hopsworks.meta_inodes_ops_datasets_deleted (inodeid, parentid, processed) (
 
-	SELECT outt.id as ds_id, outt.parent_id as parentt, 0 as processed FROM hops.hdfs_inodes outt, 
-		(SELECT i.id as parentid FROM hops.hdfs_inodes i, 
+	SELECT outt.id as ds_id, outt.parent_id as parent, 0 as processed FROM hops.hdfs_inodes outt, 
+	(SELECT i.id as parentid FROM hops.hdfs_inodes i, 
+		(SELECT inn.id AS rootid FROM hops.hdfs_inodes inn WHERE inn.parent_id = 1) AS root 
+
+	WHERE i.parent_id = root.rootid) AS parent 
+	WHERE outt.parent_id = parent.parentid LIMIT 100);
+
+-- THEN TAKE REMOVED RECORDS (OPERATION = 1)
+
+INSERT INTO hopsworks.meta_inodes_ops_datasets_deleted (inodeid, parentid, processed) (
+
+	SELECT log.inode_id as ds_id, log.dataset_id as parent, 0 as processed FROM hops.hdfs_metadata_log log,
+
+		(SELECT i.id FROM hops.hdfs_inodes i, 
 			(SELECT inn.id AS rootid FROM hops.hdfs_inodes inn WHERE inn.parent_id = 1) AS root 
 
 		WHERE i.parent_id = root.rootid) AS parent 
 
-	WHERE outt.parent_id = parent.parentid LIMIT 100)
+	WHERE log.operation = 1 AND log.dataset_id = parent.id LIMIT 100);
 
 -- SELECT ALL DATASETS (FIRST LEVEL CHILDREN) THAT HAVE BEEN ADDED (OPERATION 0)
 
-SELECT ds._id, ds.name, ds.inode_id, ds.dataset_id as parentid, ds.description, ds.operation, metadata.EXTENDED_METADATA 
+SELECT ds._id, ds.name, ds.inode_id, ds.dataset_id as parentid, ds.description, ds.operation, "dataset" as type, metadata.EXTENDED_METADATA 
 FROM (
 	SELECT DISTINCT composite.*, dt.inode_pid, dt.inode_name, dt.description 
 	FROM hopsworks.dataset dt,
@@ -61,10 +75,11 @@ ON metadata.inodeid = ds._id ORDER BY parentid ASC
 
 -- SELECT ALL DATASETS THAT HAVE BEEN DELETED/RENAMED (OPERATION 1) AND ARE NOT YET PROCESSED
 
-SELECT ops.inode_id as _id, ops.dataset_id as _parent, ops.* 
-FROM hops.hdfs_metadata_log ops
+SELECT ops.inode_id as _id, ops.dataset_id as parentid, ops.* 
+FROM hops.hdfs_metadata_log ops 
 
-WHERE ops.operation = 1
+WHERE ops.operation = 1 
+
 AND ops.inode_id IN (SELECT inode_id FROM hopsworks.meta_inodes_ops_datasets_deleted WHERE processed = 0)
 
 
