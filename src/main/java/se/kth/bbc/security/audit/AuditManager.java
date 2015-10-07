19517a3fa6e007d731b222bd303dff4bc51ed108
Ali Gholami <gholami@sics.se>
2015-10-07 09:32:45 +0200

    Added broken dependencies

---
+++ src/main/java/se/kth/bbc/security/audit/AuditManager.java
@@ -0,0 +1,283 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kth.bbc.security.audit;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import se.kth.bbc.security.audit.model.AccountAudit;
+import se.kth.bbc.security.audit.model.RolesAudit;
+import se.kth.bbc.security.audit.model.Userlogins;
+import se.kth.bbc.security.ua.model.User;
+
+/**
+ *
+ * @author Ali Gholami <gholami@pdc.kth.se>
+ */
+@Stateless
+public class AuditManager {
+
+  @PersistenceContext(unitName = "kthfsPU")
+  private EntityManager em;
+
+  public Userlogins getLastUserLogin(int uid) {
+    String sql = "SELECT * FROM userlogins  WHERE uid=" + uid
+            + " ORDER BY login_date DESC LIMIT 1 OFFSET 2";
+    Query query = em.createNativeQuery(sql, Userlogins.class);
+    query.setMaxResults(2);
+
+    List<Userlogins> ul = query.getResultList();
+
+    if (ul.isEmpty()) {
+      return null;
+    }
+
+    if (ul.size() == 1) {
+      return ul.get(0);
+    }
+
+    return ul.get(1);
+
+  }
+
+  public List<Userlogins> getUsersLoginsFromTo(Date from, Date to, String action) {
+
+    String sql = "SELECT * FROM userlogins  WHERE  (login_date >= '" + from
+            + "' AND login_date <='" + to + "' AND action ='" + action + "')";
+
+    Query query = em.createNativeQuery(sql, Userlogins.class);
+
+    List<Userlogins> ul = query.getResultList();
+
+    if (ul.isEmpty()) {
+      return null;
+    }
+
+    return ul;
+  }
+
+  public List<Userlogins> getUserLoginsFromTo(int uid, Date from, Date to,
+          String action) {
+
+    String sql = "SELECT * FROM userlogins  WHERE (uid=" + uid
+            + " AND login_date >= '" + from + "' AND login_date <='" + to
+            + "' AND action ='" + action + "')";
+
+    Query query = em.createNativeQuery(sql, Userlogins.class);
+
+    List<Userlogins> ul = query.getResultList();
+
+    if (ul.isEmpty()) {
+      return null;
+    }
+
+    return ul;
+  }
+
+  public List<AccountAudit> getAccountAudit(Date from, Date to,
+          String action) {
+
+    String sql = "SELECT * FROM account_audit WHERE ( time >='" + from
+            + "' AND time <='" + to + "' AND action ='"
+            + action + "')";
+    Query query = em.createNativeQuery(sql, AccountAudit.class);
+
+    List<AccountAudit> ul = query.getResultList();
+
+    if (ul.isEmpty()) {
+      return null;
+    }
+    return ul;
+  }
+
+  public List<AccountAudit> getAccountAudit(int uid, Date from, Date to,
+          String action) {
+
+    String sql = "SELECT * FROM account_audit WHERE (initiator=" + uid
+            + " AND time >='" + from + "' AND time <='" + to + "' AND action ='"
+            + action + "')";
+    Query query = em.createNativeQuery(sql, AccountAudit.class);
+
+    List<AccountAudit> ul = query.getResultList();
+
+    if (ul.isEmpty()) {
+      return null;
+    }
+    return ul;
+  }
+
+  public List<RolesAudit> getRoletAudit(int uid, Date from, Date to,
+          String action) {
+
+    String sql = null;
+
+    if (action.isEmpty() || action == null || action.equals("ALL")) {
+      sql = "SELECT * FROM roles_audit WHERE (target=" + uid
+              + " AND time >= '" + from + "' AND time <= '" + to
+              + "')";
+    } else {
+
+      sql = "SELECT * FROM roles_audit WHERE (target=" + uid
+              + " AND time >= '" + from + "' AND time <= '" + to
+              + "' AND action = '"
+              + action + "')";
+    }
+
+    Query query = em.createNativeQuery(sql, RolesAudit.class);
+
+    List<RolesAudit> ul = query.getResultList();
+
+    if (ul.isEmpty()) {
+      return null;
+    }
+    return ul;
+  }
+
+  public List<RolesAudit> getRoletAudit(Date from, Date to,
+          String action) {
+
+    String sql = null;
+
+    if (action.isEmpty() || action == null || action.equals("ALL")) {
+      sql = "SELECT * FROM roles_audit WHERE ( time >= '" + from
+              + "' AND time <= '" + to + "')";
+    } else {
+      sql = "SELECT * FROM roles_audit WHERE ( time >= '" + from
+              + "' AND time <= '" + to + "' AND action = '"
+              + action + "')";
+    }
+
+    Query query = em.createNativeQuery(sql, RolesAudit.class);
+
+    List<RolesAudit> ul = query.getResultList();
+
+    if (ul.isEmpty()) {
+      return null;
+    }
+    return ul;
+  }
+
+  public List<RolesAudit> getInitiatorRoletAudit(int uid, Date from, Date to,
+          String action) {
+
+    String sql = "SELECT * FROM rolse_audit WHERE (initiator=" + uid
+            + " AND time >= '" + from + "' AND time <= '" + to
+            + "' AND action ='"
+            + action + "')";
+    Query query = em.createNativeQuery(sql, RolesAudit.class);
+
+    List<RolesAudit> ul = query.getResultList();
+
+    if (ul.isEmpty()) {
+      return null;
+    }
+    return ul;
+  }
+
+  /**
+   * Register the login information into the log storage.
+   * <p>
+   * @param u
+   * @param action
+   * @param ip
+   * @param browser
+   * @param os
+   * @param mac
+   * @param outcome
+   * @return
+   */
+  public boolean registerLoginInfo(User u, String action, String ip,
+          String browser, String os, String mac, String outcome) {
+
+    Userlogins l = new Userlogins();
+    l.setUid(u.getUid());
+    l.setBrowser(browser);
+    l.setIp(ip);
+    l.setOs(os);
+    l.setAction(action);
+    l.setOutcome(outcome);
+    l.setLoginDate(new Timestamp(new Date().getTime()));
+    l.setEmail(u.getEmail());
+    l.setMac(mac);
+    em.persist(l);
+
+    return true;
+  }
+
+  /**
+   * Register the role assignment changes.
+   * <p>
+   * @param u
+   * @param action
+   * @param ip
+   * @param browser
+   * @param os
+   * @param mac
+   * @param outcome
+   * @param message
+   * @param tar
+   * @return
+   */
+  public boolean registerRoleChange(User u, String action, String ip,
+          String browser, String os, String mac, String outcome, String message,
+          User tar) {
+
+    RolesAudit ra = new RolesAudit();
+    ra.setInitiator(u.getUid());
+    ra.setBrowser(browser);
+    ra.setIp(ip);
+    ra.setOs(os);
+    ra.setEmail(u.getEmail());
+    ra.setAction(action);
+    ra.setOutcome(outcome);
+    ra.setTime(new Timestamp(new Date().getTime()));
+    ra.setEmail(u.getEmail());
+    ra.setMac(mac);
+    ra.setMessage(message);
+    ra.setTarget(tar);
+    em.persist(ra);
+
+    return true;
+  }
+
+  /**
+   * Register the account update info.
+   * <p>
+   * @param u
+   * @param action
+   * @param ip
+   * @param browser
+   * @param os
+   * @param mac
+   * @param outcome
+   * @param message
+   * @return
+   */
+  public boolean registerAccountChange(User u, String action, String ip,
+          String browser, String os, String mac, String outcome, String message
+  ) {
+
+    AccountAudit aa = new AccountAudit();
+    aa.setInitiator(u);
+    aa.setBrowser(browser);
+    aa.setIp(ip);
+    aa.setOs(os);
+    aa.setAction(action);
+    aa.setOutcome(outcome);
+    aa.setMessage(message);
+    aa.setTime(new Timestamp(new Date().getTime()));
+    aa.setMac(mac);
+    aa.setEmail(u.getEmail());
+    em.persist(aa);
+
+    return true;
+  }
+
+}
---
+++ src/main/java/se/kth/bbc/security/audit/model/AccountAudit.java
@@ -0,0 +1,218 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kth.bbc.security.audit.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import se.kth.bbc.security.ua.model.User;
+
+/**
+ *
+ * @author Ali Gholami <gholami@pdc.kth.se>
+ */
+@Entity
+@Table(name = "account_audit")
+@XmlRootElement
+@NamedQueries({
+  @NamedQuery(name = "AccountAudit.findAll",
+          query = "SELECT a FROM AccountAudit a"),
+  @NamedQuery(name = "AccountAudit.findByLogId",
+          query = "SELECT a FROM AccountAudit a WHERE a.logId = :logId"),
+  @NamedQuery(name = "AccountAudit.findByAction",
+          query = "SELECT a FROM AccountAudit a WHERE a.action = :action"),
+  @NamedQuery(name = "AccountAudit.findByTime",
+          query = "SELECT a FROM AccountAudit a WHERE a.time = :time"),
+  @NamedQuery(name = "AccountAudit.findByMessage",
+          query = "SELECT a FROM AccountAudit a WHERE a.message = :message"),
+  @NamedQuery(name = "AccountAudit.findByOutcome",
+          query = "SELECT a FROM AccountAudit a WHERE a.outcome = :outcome"),
+  @NamedQuery(name = "AccountAudit.findByIp",
+          query = "SELECT a FROM AccountAudit a WHERE a.ip = :ip"),
+  @NamedQuery(name = "AccountAudit.findByEmail",
+          query = "SELECT a FROM AccountAudit a WHERE a.email = :email"),
+  @NamedQuery(name = "AccountAudit.findByBrowser",
+          query = "SELECT a FROM AccountAudit a WHERE a.browser = :browser"),
+  @NamedQuery(name = "AccountAudit.findByOs",
+          query = "SELECT a FROM AccountAudit a WHERE a.os = :os"),
+  @NamedQuery(name = "AccountAudit.findByMac",
+          query = "SELECT a FROM AccountAudit a WHERE a.mac = :mac")})
+public class AccountAudit implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Basic(optional = false)
+  @Column(name = "log_id")
+  private Integer logId;
+  @Size(max = 45)
+  @Column(name = "action")
+  private String action;
+  @Column(name = "time")
+  @Temporal(TemporalType.DATE)
+  private Date time;
+  @Size(max = 100)
+  @Column(name = "message")
+  private String message;
+  @Size(max = 45)
+  @Column(name = "outcome")
+  private String outcome;
+  @Size(max = 45)
+  @Column(name = "ip")
+  private String ip;
+  @Size(max = 45)
+  @Column(name = "browser")
+  private String browser;
+  @Size(max = 45)
+  @Column(name = "os")
+  private String os;
+  @Size(max = 254)
+  @Column(name = "email")
+  private String email;
+  @Size(max = 45)
+  @Column(name = "mac")
+  private String mac;
+  @JoinColumn(name = "initiator",
+          referencedColumnName = "uid")
+  @ManyToOne
+  private User initiator;
+
+  public AccountAudit() {
+  }
+
+  public AccountAudit(Integer logId) {
+    this.logId = logId;
+  }
+
+  public Integer getLogId() {
+    return logId;
+  }
+
+  public void setLogId(Integer logId) {
+    this.logId = logId;
+  }
+
+  public String getAction() {
+    return action;
+  }
+
+  public void setAction(String action) {
+    this.action = action;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Date getTime() {
+    return time;
+  }
+
+  public void setTime(Date time) {
+    this.time = time;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public String getOutcome() {
+    return outcome;
+  }
+
+  public void setOutcome(String outcome) {
+    this.outcome = outcome;
+  }
+
+  public String getIp() {
+    return ip;
+  }
+
+  public void setIp(String ip) {
+    this.ip = ip;
+  }
+
+  public String getBrowser() {
+    return browser;
+  }
+
+  public void setBrowser(String browser) {
+    this.browser = browser;
+  }
+
+  public String getOs() {
+    return os;
+  }
+
+  public void setOs(String os) {
+    this.os = os;
+  }
+
+  public String getMac() {
+    return mac;
+  }
+
+  public void setMac(String mac) {
+    this.mac = mac;
+  }
+
+  public User getInitiator() {
+    return initiator;
+  }
+
+  public void setInitiator(User initiator) {
+    this.initiator = initiator;
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 0;
+    hash += (logId != null ? logId.hashCode() : 0);
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    // TODO: Warning - this method won't work in the case the id fields are not set
+    if (!(object instanceof AccountAudit)) {
+      return false;
+    }
+    AccountAudit other = (AccountAudit) object;
+    if ((this.logId == null && other.logId != null) || (this.logId != null
+            && !this.logId.equals(other.logId))) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "se.kth.bb.security.audit.model.AccountAudit[ logId=" + logId + " ]";
+  }
+
+}
