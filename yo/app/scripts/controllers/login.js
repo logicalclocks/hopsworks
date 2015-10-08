16c2a6aa19bda075e61bc52f39d280df9b879205
Gayana <gayanachandrasekara@gmail.com>
2015-10-08 17:03:26 +0200

    update

--- yo/app/scripts/controllers/login.js
+++ yo/app/scripts/controllers/login.js
@@ -15,7 +15,7 @@ angular.module('hopsWorksApp')
                         self.working = false;
                         $cookies.email = self.user.email;
                         $location.path('/');
-						location.reload();
+			location.reload();
                       }, function (error) {
                         self.working = false;
                         self.errorMessage = error.data.errorMsg;
