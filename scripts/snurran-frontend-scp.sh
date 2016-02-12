78b959b1c2e6ea45e721758b2105a2bfe3e51495
Jim Dowling <jdowling@sics.se>
2016-02-12 09:35:56 +0100

    script for deploying to snurran for debug

---
+++ scripts/snurran-frontend-scp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Deploy the frontend to the glassfish home directory and run bower
+export PORT=9191
+export WEBPORT=8080
+export SERVER=snurran.sics.se
+export key=private_key
+
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o IdentitiesOnly=yes -i $key -p $PORT vagrant@${SERVER} "cd /srv/glassfish/domain1 && sudo chown -R glassfish:vagrant docroot && sudo chmod -R 775 *"
+
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o IdentitiesOnly=yes -i $key -P ${PORT} -r ../yo/app/ vagrant@${SERVER}:/srv/glassfish/domain1/docroot
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o IdentitiesOnly=yes -i $key -P ${PORT} ../yo/bower.json vagrant@${SERVER}:/srv/glassfish/domain1/docroot/app
+
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o IdentitiesOnly=yes -i $key -p $PORT vagrant@${SERVER} "cd /srv/glassfish/domain1/docroot/app && bower install && perl -pi -e \"s/getLocationBase\(\)/'http:\/\/${SERVER}:${WEBPORT}\/hopsworks'/g\" scripts/services/RequestInterceptorService.js"
+
+google-chrome -new-tab http://${SERVER}:$WEBPORT/app
