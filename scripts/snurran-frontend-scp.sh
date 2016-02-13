2fe6eafd456d95b01664216a27dce9a25cea4eda
%github_username% <jdowling@sics.se>
2016-02-13 10:45:32 +0100

    updated script for jim

--- scripts/snurran-frontend-scp.sh
+++ scripts/snurran-frontend-scp.sh
@@ -5,6 +5,8 @@ export WEBPORT=8080
 export SERVER=snurran.sics.se
 export key=private_key
 
+scp jdowling@snurran.sics.se:/home/jdowling/NetBeansProjects/hopsworks-chef/.kitchen/kitchen-vagrant/default-ubuntu-1404/.vagrant/machines/default/virtualbox/private_key .
+
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o IdentitiesOnly=yes -i $key -p $PORT vagrant@${SERVER} "cd /srv/glassfish/domain1 && sudo chown -R glassfish:vagrant docroot && sudo chmod -R 775 *"
 
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o IdentitiesOnly=yes -i $key -P ${PORT} -r ../yo/app/ vagrant@${SERVER}:/srv/glassfish/domain1/docroot
