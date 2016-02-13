ddf1ffb85499465ba16c339baae3136d784a1544
Jim Dowling <jdowling@sics.se>
2016-02-13 08:16:51 +0100

    remove ng-pattern check for new folder names - broken

--- yo/test/karma.conf.js
+++ yo/test/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
-      'bower_components/jquery/jquery.js',
+      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-animate/angular-animate.js',
