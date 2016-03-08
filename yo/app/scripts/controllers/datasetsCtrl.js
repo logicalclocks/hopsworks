01a0d3f91490337252089e2bb029d6efb02d18ce
Jim Dowling <jdowling@sics.se>
2016-03-09 00:20:11 +0100

    fixed nav-window of 1330px for testing in ds browsers. Looks ok

--- yo/app/scripts/controllers/datasetsCtrl.js
+++ yo/app/scripts/controllers/datasetsCtrl.js
@@ -29,6 +29,10 @@ angular.module('hopsWorksApp')
             self.availableTemplates = [];
             self.closeSlider = false;
 
+            $scope.toggleState = function () {
+                $scope.tgState = !$scope.tgState;
+            }
+
             $scope.sort = function (keyname) {
               $scope.sortKey = keyname;   //set the sortKey to the param passed
               $scope.reverse = !$scope.reverse; //if true make it false and vice versa
