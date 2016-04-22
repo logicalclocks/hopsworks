1619e5c2f38914dc30b84dfecfa8bcb2cd0a294d
paulmaelzer <paul.maelzer@eim.tu-berlin.de>
2016-04-22 16:09:27 +0200

    verify csr, introduced user:AGENT

--- src/main/java/se/kth/hopsworks/util/PKIUtils.java
+++ src/main/java/se/kth/hopsworks/util/PKIUtils.java
@@ -10,68 +10,94 @@ import java.util.List;
 import java.util.logging.Logger;
 import org.apache.commons.io.FileUtils;
 
-
 public class PKIUtils {
 
-    final static String PASSWORD = "changeit";
-    
-    final static Logger logger = Logger.getLogger(PKIUtils.class.getName());
+  final static String PASSWORD = "changeit";
 
-    public static String signWithServerCertificate(String csr) throws IOException, InterruptedException {
+  final static Logger logger = Logger.getLogger(PKIUtils.class.getName());
 
-        if(verifyCSR(csr)) {
-           return signCSR(csr);
-        } 
-        return null;
+  public static String signWithServerCertificate(String csr) throws IOException, InterruptedException {
+    File csrFile = File.createTempFile(System.getProperty("java.io.tmpdir"), ".csr");
+    FileUtils.writeStringToFile(csrFile, csr);
+
+    if (verifyCSR(csrFile)) {
+      return signCSR(csrFile);
     }
-    
-    private static boolean verifyCSR(String csr) {
-        //TODO
+    return null;
+  }
+
+  private static boolean verifyCSR(File csr) throws IOException, InterruptedException {
+
+    logger.info("Verifying CSR...");
+    List<String> cmds = new ArrayList<>();
+
+    cmds.add("openssl");
+    cmds.add("req");
+    cmds.add("-in");
+    cmds.add(csr.getAbsolutePath());
+    cmds.add("-noout");
+    cmds.add("-verify");
+
+    Process process = new ProcessBuilder(cmds).directory(new File("/usr/bin/")).
+            redirectErrorStream(true).start();
+    BufferedReader br = new BufferedReader(new InputStreamReader(
+            process.getInputStream(), Charset.forName("UTF8")));
+    String line;
+    while ((line = br.readLine()) != null) {
+      if (line.equalsIgnoreCase("verify failure")) {
+        logger.info("verify failure");
         return false;
+      } else if (line.equalsIgnoreCase("verify OK")) {
+        logger.info("verify OK");
+        return true;
+      }
     }
-
-    //Attention, expects .csr as a formatted String!
-    private static String signCSR(String csr) throws IOException, InterruptedException{
-        File csrFile = File.createTempFile(System.getProperty("java.io.tmpdir"), ".csr");
-        FileUtils.writeStringToFile(csrFile, csr);
-
-        File generatedCertFile = File.createTempFile(System.getProperty("java.io.tmpdir"), ".cert.pem");
-        
-        logger.info("Signing CSR...");
-        List<String> cmds = new ArrayList<>();
-        
-        cmds.add("openssl");
-        cmds.add("ca");
-        cmds.add("-batch");
-        cmds.add("-config");
-        cmds.add(Settings.CA_DIR +"openssl.cnf");
-        cmds.add("-passin");
-        cmds.add("pass:"+PASSWORD);
-        cmds.add("-extensions");
-        cmds.add("usr_cert");
-        cmds.add("-days");
-        cmds.add("365");
-        cmds.add("-notext");
-        cmds.add("-md");
-        cmds.add("sha256");
-        cmds.add("-in");
-        cmds.add(csrFile.getAbsolutePath());
-        cmds.add("-out");
-        cmds.add(generatedCertFile.getAbsolutePath());
-        
-        Process process = new ProcessBuilder(cmds).directory(new File("/usr/bin/")).
-                redirectErrorStream(true).start();
-        BufferedReader br = new BufferedReader(new InputStreamReader(
-                process.getInputStream(), Charset.forName("UTF8")));
-        String line;
-        while ((line = br.readLine()) != null) {
-            logger.info(line);
-        }
-        process.waitFor();
-        if (process.exitValue() != 0) {
-            throw new RuntimeException("Failed to sign certificate.");
-        }
-        logger.info("Singned certificate.");
-        return FileUtils.readFileToString(generatedCertFile);
+    process.waitFor();
+    if (process.exitValue() != 0) {
+      throw new RuntimeException("failed to verify csr");
     }
+    return false;
+  }
+
+  private static String signCSR(File csr) throws IOException, InterruptedException {
+
+    File generatedCertFile = File.createTempFile(System.getProperty("java.io.tmpdir"), ".cert.pem");
+
+    logger.info("Signing CSR...");
+    List<String> cmds = new ArrayList<>();
+
+    cmds.add("openssl");
+    cmds.add("ca");
+    cmds.add("-batch");
+    cmds.add("-config");
+    cmds.add(Settings.CA_DIR + "openssl.cnf");
+    cmds.add("-passin");
+    cmds.add("pass:" + PASSWORD);
+    cmds.add("-extensions");
+    cmds.add("usr_cert");
+    cmds.add("-days");
+    cmds.add("365");
+    cmds.add("-notext");
+    cmds.add("-md");
+    cmds.add("sha256");
+    cmds.add("-in");
+    cmds.add(csr.getAbsolutePath());
+    cmds.add("-out");
+    cmds.add(generatedCertFile.getAbsolutePath());
+
+    Process process = new ProcessBuilder(cmds).directory(new File("/usr/bin/")).
+            redirectErrorStream(true).start();
+    BufferedReader br = new BufferedReader(new InputStreamReader(
+            process.getInputStream(), Charset.forName("UTF8")));
+    String line;
+    while ((line = br.readLine()) != null) {
+      logger.info(line);
+    }
+    process.waitFor();
+    if (process.exitValue() != 0) {
+      throw new RuntimeException("Failed to sign certificate.");
+    }
+    logger.info("Singned certificate.");
+    return FileUtils.readFileToString(generatedCertFile);
+  }
 }
