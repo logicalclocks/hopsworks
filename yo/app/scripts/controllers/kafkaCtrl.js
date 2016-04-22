a929a31fd2fe1617bbcdc9a6b5489af99781fd61
Jim Dowling <jdowling@sics.se>
2016-04-22 23:55:13 +0200

    topic html, ctrl, and service addded for frontend - only scaffolding

--- yo/app/scripts/controllers/kafkaCtrl.js
+++ yo/app/scripts/controllers/kafkaCtrl.js
@@ -24,42 +24,26 @@ angular.module('hopsWorksApp')
               });
             };
 
-            getAllTopics();
+            self.getAllTopics();
 
 
             /**
              * Navigate to the new job page.
              * @returns {undefined}
              */
-            self.createTopic = function () {
+            self.createTopic = function (topicName) {
               KafkaService.createTopic(self.projectId, topicName).then(
                       function (success) {
-                        getTopics();
+                       self.getAllTopics();
                       }, function (error) {
                 growl.error(error.data.errorMsg, {title: 'Failed to create topic', ttl: 5000});
               });
-
-
-//              StorageService.clear();
-//              $location.path('project/' + self.projectId + '/newjob');
-            };
-
-            self.showTopicDetails = function (topic) {
-              KafkaService.getTopicDetails  (self.projectId, topicName).then(
-                      function (success) {
-                        getTopics();
-                      }, function (error) {
-                growl.error(error.data.errorMsg, {title: 'Failed to get topic details', ttl: 5000});
-              });
-            };
-
-
-            self.removeTopic = function (topicName) {
+            self.removeTopic = function (topicId) {
               ModalService.confirm("sm", "Delete Topic (" + topicName + ")",
                       "Do you really want to delete this topic?\n\
                                 This action cannot be undone.")
                       .then(function (success) {
-                        KafkaService.removeTopic(self.projectId, topicName).then(
+                        KafkaService.removeTopic(self.projectId, topicId).then(
                                 function (success) {
                                   getTopics();
                                 }, function (error) {
@@ -71,6 +55,37 @@ angular.module('hopsWorksApp')
             };
 
 
+//              StorageService.clear();
+//              $location.path('project/' + self.projectId + '/newjob');
+            };
+
+            self.getAclsForTopic = function (topicId) {
+              KafkaService.getAclsForTopic  (self.projectId, topicId).then(
+                      function (success) {
+                        topics[topicId] = success.data;
+                      }, function (error) {
+                growl.error(error.data.errorMsg, {title: 'Failed to get topic details', ttl: 5000});
+              });
+            };
+
+            self.addAcl = function (topicId, acl) {
+                        KafkaService.addAcl(self.projectId, topicId, acl).then(
+                                function (success) {
+                                  self.getAclsForTopic();
+                                }, function (error) {
+                          growl.error(error.data.errorMsg, {title: 'Failed to remove topic', ttl: 5000});
+                        });
+
+            };
+            self.removeAcl = function (topicId, aclId) {
+                        KafkaService.removeTopicAcl(self.projectId, topicId, aclId).then(
+                                function (success) {
+                                  self.getAclsForTopic();
+                                }, function (error) {
+                          growl.error(error.data.errorMsg, {title: 'Failed to remove topic', ttl: 5000});
+                        });
+
+            };
 
           }]);
 
---
+++ yo/app/scripts/services/KafkaService.js
@@ -0,0 +1,84 @@
+'use strict';
+/*
+ * Service allowing fetching topic history objects by type.
+ */
+angular.module('hopsWorksApp')
+
+        .factory('KafkaService', ['$http', function ($http) {
+            var service = {
+              /**
+               * Get all the topics defined in the project with given id.
+               * @param {int} projectId
+               * @returns {unresolved} A list of topic objects.
+               */
+              getTopics: function (projectId) {
+                return $http.get('/api/project/' + projectId + '/topics');
+              },
+              /**
+               * Get the details of the topic with given ID, under the given project.
+               * Includes all ACLs for the topic
+               * @param {type} projectId
+               * @param {type} topicId
+               * @returns {unresolved} A complete description of the requested topic.
+               */
+              getAclsForTopic: function (projectId, topicId) {
+                return $http.get('/api/project/' + projectId + '/topics/' + topicId);
+              },
+              /**
+               * Create a new Topic in the given project, of the given type. 
+               * @param {type} projectId 
+               * @param {type} type
+               * @param {type} topicDetails The configuration of the newly created topic.
+               * @returns {undefined} The newly created topic object.
+               */
+              createTopic: function (projectId, topicDetails) {
+                var req = {
+                  method: 'POST',
+                  url: '/api/project/' + projectId + '/topic',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  data: topicDetails
+                };
+                return $http(req);
+              },
+              /**
+               * Add a new ACL rule to a Topic in the given project. 
+               * @param {type} projectId 
+               * @param {type} topicId
+               * @param {type} topicAcl The ACL for the topic.
+               * @returns {undefined} The newly created topic object.
+               */
+              addAcl: function (projectId, topicId, topicAcl) {
+                var req = {
+                  method: 'POST',
+                  url: '/api/project/' + projectId + '/topics/' + topicId + "/addAcl",
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  data: topicAcl
+                };
+                return $http(req);
+              },
+              /**
+               * Delete an ACL rule for a topic
+               * @param {type} projectId
+               * @param {type} topicId
+               * @returns {undefined} true if success, false otheriwse
+               */
+              removeAcl: function (projectId, topicId, aclId) {
+                return $http.delete('/api/project/' + projectId + '/topics/' + topicId + '/deleteAcl/' + aclId);
+              },
+              /**
+               * Delete a topic 
+               * @param {type} projectId
+               * @param {type} topicId
+               * @returns {undefined} true if success, false otheriwse
+               */
+              removeTopic: function (projectId, topicId) {
+                return $http.delete('/api/project/' + projectId + '/topics/' + topicId + '/deleteTopic');
+              }
+              
+            };
+            return service;
+          }]);
