3501dc705bc03a60f937846dda6d332da3f11f8e
Jim Dowling <jdowling@sics.se>
2016-05-13 09:33:39 +0200

    updated versions for grunt packages

--- yo/test/karma.conf.js
+++ yo/test/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
     files: [
       // bower:js
       'bower_components/angular/angular.js',
-      'bower_components/jquery/jquery.js',
+      'bower_components/jquery/dist/jquery.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-aria/angular-aria.js',
