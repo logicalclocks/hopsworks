a955c1453c25bf378b50ae09f6ebbc760ed9f3c3
Jim Dowling <jdowling@sics.se>
2016-05-27 01:14:44 +0200

    revert upload service to original one

--- src/main/java/se/kth/hopsworks/rest/UploadService.java
+++ src/main/java/se/kth/hopsworks/rest/UploadService.java
@@ -1,7 +1,5 @@
 package se.kth.hopsworks.rest;
 
-import io.hops.hdfs.HdfsLeDescriptors;
-import io.hops.hdfs.HdfsLeDescriptorsFacade;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -40,7 +38,6 @@ import se.kth.hopsworks.controller.ResponseMessages;
 import se.kth.hopsworks.filters.AllowedRoles;
 import se.kth.hopsworks.hdfs.fileoperations.DistributedFsService;
 import se.kth.hopsworks.hdfs.fileoperations.DistributedFileSystemOps;
-import se.kth.hopsworks.hdfsUsers.controller.HdfsUsersController;
 import se.kth.hopsworks.meta.db.InodeBasicMetadataFacade;
 import se.kth.hopsworks.meta.db.TemplateFacade;
 import se.kth.hopsworks.meta.entity.InodeBasicMetadata;
@@ -49,14 +46,13 @@ import se.kth.hopsworks.meta.exception.ApplicationException;
 import se.kth.hopsworks.meta.exception.DatabaseException;
 import se.kth.hopsworks.meta.wscomm.ResponseBuilder;
 import se.kth.hopsworks.meta.wscomm.message.UploadedTemplateMessage;
-import se.kth.hopsworks.util.Settings;
 
 @RequestScoped
 @TransactionAttribute(TransactionAttributeType.NEVER)
 public class UploadService {
 
   private final static Logger logger = Logger.getLogger(UploadService.class.
-      getName());
+          getName());
 
   @EJB
   private NoCacheResponse noCacheResponse;
@@ -74,8 +70,6 @@ public class UploadService {
   private InodeBasicMetadataFacade basicMetaFacade;
   @EJB
   private DistributedFsService dfs;
-  @EJB
-  private HdfsLeDescriptorsFacade hdfsLeDescriptorFacade;
 
   private String path;
   private String username;
@@ -89,21 +83,21 @@ public class UploadService {
    * Sets the upload path for the file to be uploaded.
    * <p/>
    * @param uploadPath starting with Projects/projectName/...
-   * @throws AppException if there is a folder name that is not valid in the given path, the path is empty, or project
-   * name was not found.
+   * @throws AppException if there is a folder name that is not valid in
+   * the given path, the path is empty, or project name was not found.
    */
   public void setPath(String uploadPath) throws AppException {
     boolean exist = true;
     String[] pathArray = uploadPath.split(File.separator);
     if (pathArray.length < 3) { // if path does not contain project name.
       throw new AppException(Response.Status.BAD_REQUEST.getStatusCode(),
-          "Not a valid path!");
+              "Not a valid path!");
     }
     //check if all the non existing dir names in the path are valid.
     Inode parent = inodes.getProjectRoot(pathArray[2]);
     if (parent == null) {
       throw new AppException(Response.Status.BAD_REQUEST.getStatusCode(),
-          ResponseMessages.PROJECT_NOT_FOUND);
+              ResponseMessages.PROJECT_NOT_FOUND);
     }
 
     for (int i = 3; i < pathArray.length; i++) {
@@ -125,9 +119,10 @@ public class UploadService {
   }
 
   /**
-   * Sets the path for the file to be uploaded. It does not require a project name since the file to be uploaded is a
-   * template schema, irrelevant to any project or dataset. The only requirement is that the upload has to be performed
-   * in the Uploads directory
+   * Sets the path for the file to be uploaded. It does not require a project
+   * name since the file to be uploaded is a template schema, irrelevant to any
+   * project or dataset. The only requirement is that the upload has to be
+   * performed in the Uploads directory
    * <p/>
    * @param path
    * @throws se.kth.hopsworks.rest.AppException
@@ -136,7 +131,7 @@ public class UploadService {
     String[] pathArray = path.substring(1).split(File.separator);
     if (pathArray.length < 2) { // if path does not contain project name.
       throw new AppException(Response.Status.BAD_REQUEST.getStatusCode(),
-          "Not a valid path!");
+              "Not a valid path!");
     }
 
     //check if the parent directory exists. If it doesn't create it first
@@ -145,8 +140,8 @@ public class UploadService {
         fileOps.mkDir(File.separator + pathArray[0]);
       } catch (IOException e) {
         throw new AppException(Response.Status.INTERNAL_SERVER_ERROR.
-            getStatusCode(),
-            "Upload directory could not be created in the file system");
+                getStatusCode(),
+                "Upload directory could not be created in the file system");
       }
     }
 
@@ -167,8 +162,8 @@ public class UploadService {
   @Produces(MediaType.APPLICATION_JSON)
   @AllowedRoles(roles = {AllowedRoles.DATA_SCIENTIST, AllowedRoles.DATA_OWNER})
   public Response testMethod(
-      @Context HttpServletRequest request)
-      throws AppException, IOException, AccessControlException {
+          @Context HttpServletRequest request)
+          throws AppException, IOException, AccessControlException {
     String fileName = request.getParameter("flowFilename");
     Inode parent;
     JsonResponse json = new JsonResponse();
@@ -178,7 +173,7 @@ public class UploadService {
         parent = inodes.findByParentAndName(this.fileParent, fileName);
         if (parent != null) {
           throw new AppException(Response.Status.BAD_REQUEST.getStatusCode(),
-              ResponseMessages.FILE_NAME_EXIST);
+                  ResponseMessages.FILE_NAME_EXIST);
         }
       }
       //test if the user have permission to create a file in the path.
@@ -186,23 +181,23 @@ public class UploadService {
       if (this.username != null) {
         try {
           dfs.getDfsOps(username).touchz(new org.apache.hadoop.fs.Path(this.path
-              + fileName));
+                  + fileName));
         } catch (AccessControlException ex) {
           throw new AccessControlException(
-              "Permission denied: You can not upload to this folder. ");
+                  "Permission denied: You can not upload to this folder. ");
         }
       }
     }
     ResumableInfo info = getResumableInfo(request, this.path, this.templateId);
     if (info.isUploaded(new ResumableInfo.ResumableChunkNumber(
-        resumableChunkNumber))) {
+            resumableChunkNumber))) {
       json.setSuccessMessage("Uploaded");//This Chunk has been Uploaded.
       return noCacheResponse.getNoCacheResponseBuilder(Response.Status.OK).
-          entity(json).build();
+              entity(json).build();
     } else {
       json.setErrorMsg("Not uploaded");
       return noCacheResponse.getNoCacheResponseBuilder(
-          Response.Status.NO_CONTENT).entity(json).build();
+              Response.Status.NO_CONTENT).entity(json).build();
     }
 
   }
@@ -212,32 +207,32 @@ public class UploadService {
   @Produces(MediaType.APPLICATION_JSON)
   @AllowedRoles(roles = {AllowedRoles.DATA_SCIENTIST, AllowedRoles.DATA_OWNER})
   public Response uploadMethod(
-      @FormDataParam("file") InputStream uploadedInputStream,
-      @FormDataParam("file") FormDataContentDisposition fileDetail,
-      @FormDataParam("flowChunkNumber") String flowChunkNumber,
-      @FormDataParam("flowChunkSize") String flowChunkSize,
-      @FormDataParam("flowCurrentChunkSize") String flowCurrentChunkSize,
-      @FormDataParam("flowFilename") String flowFilename,
-      @FormDataParam("flowIdentifier") String flowIdentifier,
-      @FormDataParam("flowRelativePath") String flowRelativePath,
-      @FormDataParam("flowTotalChunks") String flowTotalChunks,
-      @FormDataParam("flowTotalSize") String flowTotalSize)
-      throws AppException, IOException {
+          @FormDataParam("file") InputStream uploadedInputStream,
+          @FormDataParam("file") FormDataContentDisposition fileDetail,
+          @FormDataParam("flowChunkNumber") String flowChunkNumber,
+          @FormDataParam("flowChunkSize") String flowChunkSize,
+          @FormDataParam("flowCurrentChunkSize") String flowCurrentChunkSize,
+          @FormDataParam("flowFilename") String flowFilename,
+          @FormDataParam("flowIdentifier") String flowIdentifier,
+          @FormDataParam("flowRelativePath") String flowRelativePath,
+          @FormDataParam("flowTotalChunks") String flowTotalChunks,
+          @FormDataParam("flowTotalSize") String flowTotalSize)
+          throws AppException, IOException {
 
     JsonResponse json = new JsonResponse();
 
     int resumableChunkNumber = HttpUtils.toInt(flowChunkNumber, -1);
     ResumableInfo info = getResumableInfo(flowChunkSize, flowFilename,
-        flowIdentifier, flowRelativePath, flowTotalSize, this.path,
-        this.templateId);
+            flowIdentifier, flowRelativePath, flowTotalSize, this.path,
+            this.templateId);
     String fileName = info.getResumableFilename();
     int templateid = info.getResumableTemplateId();
 
     long content_length;
     //Seek to position
     try (RandomAccessFile raf
-        = new RandomAccessFile(info.getResumableFilePath(), "rw");
-        InputStream is = uploadedInputStream) {
+            = new RandomAccessFile(info.getResumableFilePath(), "rw");
+            InputStream is = uploadedInputStream) {
       //Seek to position
       raf.seek((resumableChunkNumber - 1) * (long) info.getResumableChunkSize());
       //Save to file
@@ -258,14 +253,14 @@ public class UploadService {
 
     //Mark as uploaded and check if finished
     if (info.addChunkAndCheckIfFinished(new ResumableInfo.ResumableChunkNumber(
-        resumableChunkNumber), content_length)) { //Check if all chunks uploaded, and change filename
+            resumableChunkNumber), content_length)) { //Check if all chunks uploaded, and change filename
       ResumableInfoStorage.getInstance().remove(info);
       logger.log(Level.INFO, "All finished.");
       finished = true;
     } else {
       json.setSuccessMessage("Upload");//This Chunk has been Uploaded.
       return noCacheResponse.getNoCacheResponseBuilder(Response.Status.OK).
-          entity(json).build();
+              entity(json).build();
     }
 
     if (finished) {
@@ -276,51 +271,42 @@ public class UploadService {
         if ((this.path + fileName).endsWith(".json")) {
 
           String filePath = stagingManager.getStagingPath() + this.path
-              + fileName;
+                  + fileName;
 
           if (!Utils.checkJsonValidity(filePath)) {
             json.setErrorMsg("This was an invalid json file");
             return noCacheResponse.getNoCacheResponseBuilder(
-                Response.Status.NOT_ACCEPTABLE).entity(json).build();
+                    Response.Status.NOT_ACCEPTABLE).entity(json).build();
           }
           fileContent = Utils.getFileContents(filePath);
           JsonObject obj = Json.createReader(new StringReader(fileContent)).
-              readObject();
+                  readObject();
           String templateName = obj.getString("templateName");
           if (template.isTemplateAvailable(templateName.toLowerCase())) {
             logger.log(Level.INFO, "{0} already exists.", templateName.
-                toLowerCase());
+                    toLowerCase());
             json.setErrorMsg("Already exists.");
             return noCacheResponse.getNoCacheResponseBuilder(
-                Response.Status.NOT_ACCEPTABLE).entity(json).build();
+                    Response.Status.NOT_ACCEPTABLE).entity(json).build();
           }
 
         }
 
         this.path = Utils.ensurePathEndsInSlash(this.path);
         DistributedFileSystemOps dfsOps;
-//        if (this.username != null) {
-//          dfsOps = dfs.getDfsOps(username);
-//        } else { // to accommodate previous implimentations  
-        dfsOps = dfs.getDfsOps();
-//        }
-//        dfsOps.copyToHDFSFromLocal(true, stagingManager.
-
-        HdfsLeDescriptors hdfsLeDescriptors = hdfsLeDescriptorFacade.findEndpoint();
-
-
-        fileOps.copyToHDFSFromLocal(true, stagingManager.
-            getStagingPath()
-            + this.path + fileName, "hdfs://" + hdfsLeDescriptors.getHostname() + this.path
-            + fileName);
-
-//        fileOps.copyToHDFSFromLocal(false, file.getAbsolutePath(), "hdfs://" + hdfsLeDescriptors.getHostname() + "/Projects/" + project.getName() + "/TestJob/");
-
+        if (this.username != null) {
+          dfsOps = dfs.getDfsOps(username);
+        } else { // to accommodate previous implimentations  
+          dfsOps = dfs.getDfsOps();
+        }
+        dfsOps.copyToHDFSFromLocal(true, stagingManager.
+                getStagingPath()
+                + this.path + fileName, this.path
+                + fileName);
         org.apache.hadoop.fs.Path location = new org.apache.hadoop.fs.Path(
-            this.path + fileName);
-//        dfsOps.setPermission(location, dfsOps.getParentPermission(location)); 
-        dfsOps.setOwner(location, username, username);
-        logger.log(Level.INFO, "Copied to HDFS");
+                this.path + fileName);
+        dfsOps.setPermission(location, dfsOps.getParentPermission(location)); 
+       logger.log(Level.INFO, "Copied to HDFS");
 
         if (templateid != 0 && templateid != -1) {
           this.attachTemplateToInode(info, this.path + fileName);
@@ -342,21 +328,21 @@ public class UploadService {
         }
         json.setSuccessMessage("Successfuly uploaded file to " + this.path);
         return noCacheResponse.getNoCacheResponseBuilder(Response.Status.OK).
-            entity(json).build();
+                entity(json).build();
       } catch (AccessControlException ex) {
         throw new AccessControlException(
-            "Permission denied: You can not upload to this folder. ");
+                "Permission denied: You can not upload to this folder. ");
       } catch (IOException e) {
         logger.log(Level.INFO, "Failed to write to HDFS", e);
         json.setErrorMsg("Failed to write to HDFS");
         return noCacheResponse.getNoCacheResponseBuilder(
-            Response.Status.BAD_REQUEST).entity(json).build();
+                Response.Status.BAD_REQUEST).entity(json).build();
       }
     }
 
     json.setSuccessMessage("Uploading...");
     return noCacheResponse.getNoCacheResponseBuilder(Response.Status.OK).entity(
-        json).build();
+            json).build();
   }
 
   private void attachTemplateToInode(ResumableInfo info, String path) {
@@ -389,7 +375,7 @@ public class UploadService {
       this.responseBuilder.persistUploadedTemplate(message);
     } catch (ApplicationException e) {
       throw new AppException(Response.Status.INTERNAL_SERVER_ERROR.
-          getStatusCode(), e.getMessage());
+              getStatusCode(), e.getMessage());
     }
   }
 
@@ -398,8 +384,8 @@ public class UploadService {
   }
 
   private ResumableInfo getResumableInfo(HttpServletRequest request,
-      String hdfsPath, int templateId) throws
-      AppException {
+          String hdfsPath, int templateId) throws
+          AppException {
     //this will give us a tmp folder
     String base_dir = stagingManager.getStagingPath();
     //this will create a folder if it does not exist inside the tmp folder 
@@ -411,9 +397,9 @@ public class UploadService {
     base_dir = userTmpDir.getAbsolutePath();
 
     int resumableChunkSize = HttpUtils.toInt(request.getParameter(
-        "flowChunkSize"), -1);
+            "flowChunkSize"), -1);
     long resumableTotalSize = HttpUtils.toLong(request.getParameter(
-        "flowTotalSize"), -1);
+            "flowTotalSize"), -1);
     String resumableIdentifier = request.getParameter("flowIdentifier");
     String resumableFilename = request.getParameter("flowFilename");
     String resumableRelativePath = request.getParameter("flowRelativePath");
@@ -426,7 +412,7 @@ public class UploadService {
       try (FileReader fileReader = new FileReader(file.getAbsolutePath())) {
         fileReader.read();
         throw new AppException(Response.Status.BAD_REQUEST.getStatusCode(),
-            "A file with the same name is being uploaded.");
+                "A file with the same name is being uploaded.");
       } catch (Exception e) {
       }
     }
@@ -437,23 +423,23 @@ public class UploadService {
     ResumableInfoStorage storage = ResumableInfoStorage.getInstance();
 
     ResumableInfo info = storage.get(resumableChunkSize, resumableTotalSize,
-        resumableIdentifier, resumableFilename, resumableRelativePath,
-        resumableFilePath, templateId);
+            resumableIdentifier, resumableFilename, resumableRelativePath,
+            resumableFilePath, templateId);
     if (!info.valid()) {
       storage.remove(info);
       throw new AppException(Response.Status.BAD_REQUEST.getStatusCode(),
-          "Invalid request params.");
+              "Invalid request params.");
     }
     return info;
   }
 
   private ResumableInfo getResumableInfo(String flowChunkSize,
-      String flowFilename,
-      String flowIdentifier,
-      String flowRelativePath,
-      String flowTotalSize,
-      String hdfsPath,
-      int templateId) throws AppException {
+          String flowFilename,
+          String flowIdentifier,
+          String flowRelativePath,
+          String flowTotalSize,
+          String hdfsPath,
+          int templateId) throws AppException {
     //this will give us a tmp folder
     String base_dir = stagingManager.getStagingPath();
     //this will create a folder if it does not exist inside the tmp folder 
@@ -478,7 +464,7 @@ public class UploadService {
       try (FileReader fileReader = new FileReader(file.getAbsolutePath())) {
         fileReader.read();
         throw new AppException(Response.Status.BAD_REQUEST.getStatusCode(),
-            "A file with the same name is being uploaded.");
+                "A file with the same name is being uploaded.");
       } catch (Exception e) {
       }
     }
@@ -489,12 +475,12 @@ public class UploadService {
     ResumableInfoStorage storage = ResumableInfoStorage.getInstance();
 
     ResumableInfo info = storage.get(resumableChunkSize, resumableTotalSize,
-        resumableIdentifier, resumableFilename, resumableRelativePath,
-        resumableFilePath, templateId);
+            resumableIdentifier, resumableFilename, resumableRelativePath,
+            resumableFilePath, templateId);
     if (!info.valid()) {
       storage.remove(info);
       throw new AppException(Response.Status.BAD_REQUEST.getStatusCode(),
-          "Invalid request params.");
+              "Invalid request params.");
     }
     return info;
   }
