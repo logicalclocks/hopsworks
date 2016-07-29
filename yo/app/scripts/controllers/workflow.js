f9bb90ee3bbdba7b6ac785437f5272b8956bc394
Jim Dowling <jdowling@sics.se>
2016-07-29 16:03:00 +0200

    Added timeout so that workflow designer page loads - still .cyNavigator is not a function

--- yo/app/scripts/controllers/workflow.js
+++ yo/app/scripts/controllers/workflow.js
@@ -8,38 +8,40 @@
  * Controller of the hopsWorksApp
  */
 angular.module('hopsWorksApp')
-  .controller('WorkflowCtrl',[ '$routeParams', '$location', 'growl','WorkflowService', 'ModalService',
-      function ($routeParams, $location, growl, WorkflowService, ModalService) {
-    var self = this;
-      self.workflows = [];
-      var projectId = $routeParams.projectID;
-      var workflowId = $routeParams.workflowID;
-      var index = function(){
-          WorkflowService.index(projectId).then(function(success){
-              console.log(success);
-              self.workflows = success.data;
-          },function (error) {
-              growl.error(error.data.errorMsg, {title: 'Error', ttl: 5000})
-          })
-      }
-      index();
-
-      self.newWorkflowModal = function () {
-          ModalService.newWorkflow('md').then(
-              function (success) {
-                  index();
+        .controller('WorkflowCtrl', ['$routeParams', '$location', '$timeout', 'growl', 'WorkflowService', 'ModalService',
+          function ($routeParams, $location, $timeout, growl, WorkflowService, ModalService) {
+            var self = this;
+            self.workflows = [];
+            var projectId = $routeParams.projectID;
+            var workflowId = $routeParams.workflowID;
+            var index = function () {
+              WorkflowService.index(projectId).then(function (success) {
+                console.log(success);
+                self.workflows = success.data;
               }, function (error) {
-                  index();
-              });
-      };
-      self.goToShow = function (id) {
-          $location.path('project/' + projectId + '/workflows/' + id);
-      };
-      self.goToExecutions = function (id) {
-          $location.path('project/' + projectId + '/workflows/' + id + '/executions');
-      };
+                growl.error(error.data.errorMsg, {title: 'Error', ttl: 5000})
+              })
+            }
+            index();
 
-      if(workflowId){
-          window.workflows.init();
-      }
-  }]);
+            self.newWorkflowModal = function () {
+              ModalService.newWorkflow('md').then(
+                      function (success) {
+                        index();
+                      }, function (error) {
+                index();
+              });
+            };
+            self.goToShow = function (id) {
+              $location.path('project/' + projectId + '/workflows/' + id);
+            };
+            self.goToExecutions = function (id) {
+              $location.path('project/' + projectId + '/workflows/' + id + '/executions');
+            };
+
+            if (workflowId) {
+              $timeout(function () {
+                window.workflows.init() 
+              }, 3000);
+            }
+          }]);
