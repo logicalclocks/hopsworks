294c6ab64f148208c927c34aff8e68ef3b4cff8f
ErmiasG <ermiasg@kth.se>
2016-08-11 18:08:45 +0200

    added tests for project

--- test/spec/projects_spec.rb
+++ test/spec/projects_spec.rb
@@ -21,12 +21,33 @@ describe 'projects' do
         expect_json(successMessage: "Project created successfully.")
         expect_status(201)
       end
-
+      
       it 'should fail with invalid params' do
         post "/hopsworks/api/project", {projectName: "project_#{Time.now.to_i}"}
         expect_status(500)
       end
     end
   end
-
+  describe "#access" do
+    context 'without authentication' do
+      before :all do
+        reset_session
+      end
+      it "should fail to get project list" do
+        get "/hopsworks/api/project/getAll"
+        expect_json(errorMsg: "Client not authorized for this invocation")
+        expect_status(401)
+      end
+    end
+    context 'with authentication' do
+      before :all do
+        with_valid_session
+      end
+      it "should return project list" do
+        get "/hopsworks/api/project/getAll"
+        #expect_json()
+        expect_status(200)
+      end
+    end
+  end
 end
