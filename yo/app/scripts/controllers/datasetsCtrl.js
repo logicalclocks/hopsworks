acefd0e560b66ca98c6e611fe51f4bcad3c9664c
Jim Dowling <jdowling@sics.se>
2016-09-21 19:44:35 +0200

    updates to browser

--- yo/app/scripts/controllers/datasetsCtrl.js
+++ yo/app/scripts/controllers/datasetsCtrl.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 angular.module('hopsWorksApp')
@@ -99,11 +100,11 @@ angular.module('hopsWorksApp')
             });
 
 
-            self.filesNotSelected = function () {
+            self.filesSelected = function () {
               if ((self.selected !== null && self.selected !== undefined) || self.isSelectedFiles()) {
-                return false;
+                return true;
               }
-              return true;
+              return false;
             }
 
             self.isShared = function () {
@@ -627,9 +628,11 @@ This will make all its files unavailable to other projects unless you share it e
 
             self.haveSelected = function (file) {
 
-              if (file !== undefined && file.name !== undefined && self.fileDetail !== null &&
-                      self.fileDetail !== undefined &&
-                      (self.fileDetail.name === file.name || (file.name in self.selectedFiles))) {
+              if (file === undefined || file === null || file.name === undefined || file.name === null) {
+                return false;
+              }
+              if ((self.fileDetail !== null && self.fileDetail !== undefined && self.fileDetail.name === file.name) || 
+                      (file.name in self.selectedFiles)) {
                 return true;
               }
               return false;
