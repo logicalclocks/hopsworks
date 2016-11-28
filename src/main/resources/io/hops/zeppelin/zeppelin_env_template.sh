cea9ca5b432382b2f854081389e4f82df5030443
Theofilos Kakantousis <t.kakantousis@gmail.com>
2016-11-28 11:16:49 +0100

    Append classpath variable for zeppelin tensorflow

--- src/main/resources/io/hops/zeppelin/zeppelin_env_template.sh
+++ src/main/resources/io/hops/zeppelin/zeppelin_env_template.sh
@@ -8,4 +8,4 @@ export HADOOP_USER_NAME=%%hadoop_user%%
 export JAVA_HOME=%%java_home%%
 export HADOOP_HDFS_HOME=%%hadoop_dir%%
 export LD_LIBRARY_PATH=%%ld_library_path%%:%%java_home%%/jre/lib/amd64/server:/usr/local/cuda/lib64/
-export CLASSPATH=%%hadoop_classpath_global%%
+export CLASSPATH=%%hadoop_classpath_global%%:$CLASSPATH
