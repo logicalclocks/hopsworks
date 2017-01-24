f16fec83428d0633af20673459535e0359486f58
Jim Dowling <jdowling@sics.se>
2017-01-24 10:40:22 +0100

    updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,7 @@ set -e
 cd "$(dirname "$0")"
 VERSION=`grep -o -a -m 1 -h -r "version>.*</version" ../pom.xml | head -1 | sed "s/version//g" | sed "s/>//" | sed "s/<\///g"`
 
-echo "Deploying version ${VERSION} of hopsworks.war to http://snurran.sics.se/hops/hopsworks-${VERSION}.war"
-scp ../target/hopsworks.war glassfish@snurran.sics.se:/var/www/hops/hopsworks-${VERSION}.war
+echo "Deploying versions ${VERSION} of hopsworks war and ear files to http://snurran.sics.se/hops/"
+
+scp ../hopsworks-ear/target/hopsworks-ear.ear glassfish@snurran.sics.se:/var/www/hops/hopsworks-ear-${VERSION}.ear
+scp ../hopsworks-web/target/hopsworks-web.war glassfish@snurran.sics.se:/var/www/hops/hopsworks-web-${VERSION}.war
