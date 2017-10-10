b5c2d609f365a07d3f8584e6a95d1ae776ed46ad
Theofilos Kakantousis <tkakantousis@users.noreply.github.com>
2017-10-10 11:41:57 +0200

    Show proper error (#691)

--- hopsworks-common/src/main/java/io/hops/hopsworks/common/dataset/DatasetController.java
+++ hopsworks-common/src/main/java/io/hops/hopsworks/common/dataset/DatasetController.java
@@ -37,6 +37,7 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
+import javax.persistence.NonUniqueResultException;
 import javax.validation.ValidationException;
 import javax.ws.rs.core.Response;
 import org.apache.hadoop.fs.FSDataInputStream;
@@ -172,6 +173,10 @@ public class DatasetController {
           dfso.rm(new Path(dsPath), true);//if dataset persist fails rm ds folder.
           throw failed;
         } catch (IOException ex) {
+          if(e.getCause() instanceof NonUniqueResultException){
+            throw new IOException(
+              "A shared Dataset with the same name already exists.", ex);
+          }
           throw new IOException(
               "Failed to clean up properly on dataset creation failure", ex);
         }
