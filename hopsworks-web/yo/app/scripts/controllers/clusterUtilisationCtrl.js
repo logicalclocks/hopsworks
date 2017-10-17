c61281e44ed5a84f9f787378ce2eb2190fd96f2d
Gautier Berthou <gautier@sics.se>
2017-10-17 18:19:32 +0200

    [HOPSWORKS-249] add health indicator for YARN, HDFS and Kafka (#694)
    * [HOPSWORKS-249] add helth indicator for YARN, HDFS and Kafka
    
    * correct omission of getKafkaStatus in javascript
    
    * do not show kafka in project not having kafka enabled

--- hopsworks-web/yo/app/scripts/controllers/clusterUtilisationCtrl.js
+++ hopsworks-web/yo/app/scripts/controllers/clusterUtilisationCtrl.js
@@ -15,6 +15,9 @@ angular.module('hopsWorksApp')
             self.gpusPercent = 0;
             self.progressBarClass = 'progress-bar-success';
             self.gpuBarClass = 'progress-bar-success';
+            self.HdfsStatus = 'text-success';
+            self.YarnStatus = 'text-success';
+            self.KafkaStatus = 'text-success';
             
             var getClusterUtilisation = function () {
               ClusterUtilService.getYarnmultiplicator().then(
@@ -55,11 +58,106 @@ angular.module('hopsWorksApp')
               
             };
             
+            var getHdfsStatus = function () {
+              ClusterUtilService.getHdfsStatus().then(
+                      function (success) {
+                        var nbDataNodes = 0;
+                        var nbRunningDataNodes = 0;
+                        var nbNameNodes = 0;
+                        var nbRunningNameNodes = 0;
+                        success.data.forEach(function (status) {
+                          if (status.role === "datanode") {
+                            nbDataNodes++;
+                            if (status.status === "Started") {
+                              nbRunningDataNodes++;
+                            }
+                          }
+                          if (status.role === "namenode") {
+                            nbNameNodes++;
+                            if (status.status === "Started") {
+                              nbRunningNameNodes++;
+                            }
+                          }
+                        });
+                        if (nbRunningDataNodes > nbDataNodes * 2 / 3 && nbRunningNameNodes > nbNameNodes / 2) {
+                          self.HdfsStatus = 'status-success';
+                        } else if (nbRunningDataNodes > nbDataNodes * 1 / 3 && nbRunningNameNodes >= 1) {
+                          self.HdfsStatus = 'status-warning';
+                        } else {
+                          self.HdfsStatus = 'status-danger';
+                        }
+
+                      }, function (error) {
+                console.log("problem getting HDFS status");
+              });
+            };
+
+            var getYarnStatus = function () {
+              ClusterUtilService.getYarnStatus().then(
+                      function (success) {
+                        var nbNodeManagers = 0;
+                        var nbRunningNodeManagers = 0;
+                        var nbRMs = 0;
+                        var nbRunningRMs = 0;
+                        success.data.forEach(function (status) {
+                          if (status.role === "nodemanager") {
+                            nbNodeManagers++;
+                            if (status.status === "Started") {
+                              nbRunningNodeManagers++;
+                            }
+                          }
+                          if (status.role === "resourcemanager") {
+                            nbRMs++;
+                            if (status.status === "Started") {
+                              nbRunningRMs++;
+                            }
+                          }
+                        });
+                        if (nbRunningNodeManagers > nbNodeManagers * 2 / 3 && nbRunningRMs > nbRMs / 2) {
+                          self.YarnStatus = 'status-success';
+                        } else if (nbRunningNodeManagers > nbNodeManagers * 1 / 3 && nbRunningRMs >= 1) {
+                          self.YarnStatus = 'status-warning';
+                        } else {
+                          self.YarnStatus = 'status-danger';
+                        }
+
+                      }, function (error) {
+                console.log("problem getting HDFS status");
+              });
+            };
+
+            var getKafkaStatus = function () {
+              ClusterUtilService.getKafkaStatus().then(
+                      function (success) {
+                        var nbInstances = 0;
+                        var nbRunningInstances = 0;
+                        success.data.forEach(function (status) {
+                          nbInstances++;
+                          if (status.status === "Started") {
+                            nbRunningInstances++;
+                          }
+                        });
+                        if (nbRunningInstances > nbInstances * 2 / 3) {
+                          self.KafkaStatus = 'status-success';
+                        } else if (nbRunningInstances > nbInstances * 1 / 3) {
+                          self.KafkaStatus = 'status-warning';
+                        } else {
+                          self.KafkaStatus = 'status-danger';
+                        }
+
+                      }, function (error) {
+                console.log("problem getting HDFS status");
+              });
+            };
+            
             var getClusterUtilisationInterval = $interval(function () {
               getClusterUtilisation();
             }, 10000);
             getClusterUtilisation();
             getGpuUtilization();
+            getHdfsStatus();
+            getYarnStatus();
+            getKafkaStatus();
             $scope.$on("$destroy", function () {
               $interval.cancel(getClusterUtilisationInterval);
             });
