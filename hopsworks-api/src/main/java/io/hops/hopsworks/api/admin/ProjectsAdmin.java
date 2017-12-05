4bb3afb9c799765d14800a087479af916c3b656c
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2017-12-05 15:23:22 +0100

    REST endpoint to create project as another user (#814)
    Python client to create project as another user
    
    Change to user email instead of username

--- hopsworks-api/src/main/java/io/hops/hopsworks/api/admin/ProjectsAdmin.java
+++ hopsworks-api/src/main/java/io/hops/hopsworks/api/admin/ProjectsAdmin.java
@@ -19,10 +19,15 @@ package io.hops.hopsworks.api.admin;
 
 import io.hops.hopsworks.api.filter.NoCacheResponse;
 import io.hops.hopsworks.api.util.JsonResponse;
+import io.hops.hopsworks.common.constants.message.ResponseMessages;
 import io.hops.hopsworks.common.dao.project.Project;
 import io.hops.hopsworks.common.dao.project.ProjectFacade;
+import io.hops.hopsworks.common.dao.user.Users;
+import io.hops.hopsworks.common.dao.user.security.ua.UserManager;
 import io.hops.hopsworks.common.exception.AppException;
 import io.hops.hopsworks.common.project.ProjectController;
+import io.hops.hopsworks.common.project.ProjectDTO;
+import io.hops.hopsworks.common.util.Settings;
 import io.swagger.annotations.Api;
 
 import javax.annotation.security.RolesAllowed;
@@ -31,7 +36,9 @@ import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -39,6 +46,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -56,6 +64,8 @@ public class ProjectsAdmin {
   private ProjectController projectController;
   @EJB
   private NoCacheResponse noCacheResponse;
+  @EJB
+  private UserManager userManager;
   
   @DELETE
   @Produces(MediaType.APPLICATION_JSON)
@@ -75,5 +85,46 @@ public class ProjectsAdmin {
     response.setStatus(Response.Status.OK.toString());
     response.setSuccessMessage("Project with id " + id + " has been successfully deleted");
     return noCacheResponse.getNoCacheResponseBuilder(Response.Status.OK).entity(response).build();
+  }
+  
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  @Path("/projects/createas")
+  public Response createProjectAsUser(@Context SecurityContext sc, @Context HttpServletRequest request,
+      ProjectDTO projectDTO) throws AppException {
+    String userEmail = sc.getUserPrincipal().getName();
+    Users user = userManager.findByEmail(userEmail);
+    if (user == null || !user.getEmail().equals(Settings.SITE_EMAIL)) {
+      LOG.log(Level.WARNING, "Unauthorized or unknown user tried to create a Project as another user");
+      throw new AppException(Response.Status.FORBIDDEN.getStatusCode(),
+          ResponseMessages.AUTHENTICATION_FAILURE);
+    }
+    
+    String ownerEmail = projectDTO.getOwner();
+    if (ownerEmail == null) {
+      LOG.log(Level.WARNING, "Owner username is null");
+      throw new AppException(Response.Status.BAD_REQUEST.getStatusCode(), "Owner email cannot be null");
+    }
+    
+    Users owner = userManager.findByEmail(ownerEmail);
+    if (owner == null) {
+      LOG.log(Level.WARNING, "Owner is not in the database");
+      throw new AppException(Response.Status.BAD_REQUEST.getStatusCode(), "Unknown owner user " + ownerEmail);
+    }
+    
+    List<String> failedMembers = null;
+    projectController.createProject(projectDTO, owner, failedMembers, request.getSession().getId());
+    
+    JsonResponse response = new JsonResponse();
+    response.setStatus("201");
+    response.setSuccessMessage(ResponseMessages.PROJECT_CREATED);
+    
+    if (failedMembers != null && !failedMembers.isEmpty()) {
+      response.setFieldErrors(failedMembers);
+    }
+    
+    return noCacheResponse.getNoCacheResponseBuilder(Response.Status.CREATED)
+        .entity(response).build();
   }
 }
