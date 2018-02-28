0f788304a1733cad6c775eee2a56e19bcdc3cf96
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2018-02-28 14:23:38 +0100

    [HOPSWORKS-469-append]  Remove forgotten unused import from PR-879 (#881)

--- hopsworks-common/src/main/java/io/hops/hopsworks/common/jobs/execution/HopsJob.java
+++ hopsworks-common/src/main/java/io/hops/hopsworks/common/jobs/execution/HopsJob.java
@@ -26,7 +26,6 @@ import java.security.PrivilegedExceptionAction;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import io.hops.hopsworks.common.hdfs.HdfsUsersController;
 import io.hops.hopsworks.common.yarn.YarnClientWrapper;
 import org.apache.hadoop.security.UserGroupInformation;
 import io.hops.hopsworks.common.jobs.AsynchronousJobExecutor;
