c4795931762489bca409a70d5c552faab7afc892
robzor92 <robin.eric.andersson@gmail.com>
2018-10-09 13:12:52 +0200

    [HOPSWORKS-705] Extend TfLibMapping to support ongoing environment creation

--- hopsworks-common/src/main/java/io/hops/hopsworks/common/dao/tensorflow/TfLibMappingFacade.java
+++ hopsworks-common/src/main/java/io/hops/hopsworks/common/dao/tensorflow/TfLibMappingFacade.java
@@ -17,17 +17,30 @@
 package io.hops.hopsworks.common.dao.tensorflow;
 
 import io.hops.hopsworks.common.dao.project.Project;
+import io.hops.hopsworks.common.dao.project.ProjectFacade;
+import io.hops.hopsworks.common.dao.pythonDeps.CondaCommands;
+import io.hops.hopsworks.common.dao.pythonDeps.PythonDepsFacade;
+import io.hops.hopsworks.common.util.Settings;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Stateless
 public class TfLibMappingFacade {
 
   @PersistenceContext(unitName = "kthfsPU")
   private EntityManager em;
+  @EJB
+  private PythonDepsFacade pythonDepsFacade;
+  @EJB
+  private ProjectFacade projectFacade;
+  @EJB
+  private Settings settings;
 
   protected EntityManager getEntityManager() {
     return em;
@@ -46,10 +59,36 @@ public class TfLibMappingFacade {
   }
 
   public TfLibMapping findTfMappingForProject(Project project) {
-    return project.getPythonDepCollection().stream()
-        .filter(dep -> dep.getDependency().equals("tensorflow") || dep.getDependency().equals("tensorflow-gpu"))
-        .findAny()
-        .map(tfDep -> findByTfVersion(tfDep.getVersion()))
-        .orElse(null);
+
+    CondaCommands command = pythonDepsFacade.getOngoingEnvCreation(project);
+
+    if(command == null) {
+      return project.getPythonDepCollection().stream()
+          .filter(dep -> dep.getDependency().equals("tensorflow") || dep.getDependency().equals("tensorflow-gpu"))
+          .findAny()
+          .map(tfDep -> findByTfVersion(tfDep.getVersion()))
+          .orElse(null);
+    } else if(command.getOp().compareTo(PythonDepsFacade.CondaOp.CREATE) == 0) {
+      return findByTfVersion(settings.getTensorflowVersion());
+    } else if(command.getOp().compareTo(PythonDepsFacade.CondaOp.YML) == 0) {
+      String envYml = command.getEnvironmentYml();
+
+      Pattern tfCPUPattern = Pattern.compile("(tensorflow==\\d*.\\d*.\\d*)");
+      Matcher tfCPUMatcher = tfCPUPattern.matcher(envYml);
+
+      if(tfCPUMatcher.find()) {
+        String [] libVersionPair = tfCPUMatcher.group(0).split("==");
+        return findByTfVersion(libVersionPair[1]);
+      }
+
+      Pattern tfGPUPattern = Pattern.compile("(tensorflow-gpu==\\d*.\\d*.\\d*)");
+      Matcher tfGPUMatcher = tfGPUPattern.matcher(envYml);
+
+      if(tfGPUMatcher.find()) {
+        String [] libVersionPair = tfGPUMatcher.group(0).split("==");
+        return findByTfVersion(libVersionPair[1]);
+      }
+    }
+    return null;
   }
 }
