b51c89b39f7f0d8cbb28926cce115b0cbb274a72
robzor92 <robin.eric.andersson@gmail.com>
2018-10-10 19:57:36 +0200

    fix

--- hopsworks-api/src/main/java/io/hops/hopsworks/api/pythonDeps/PythonDepsService.java
+++ hopsworks-api/src/main/java/io/hops/hopsworks/api/pythonDeps/PythonDepsService.java
@@ -65,13 +65,6 @@ import io.hops.hopsworks.common.hdfs.DistributedFsService;
 import io.hops.hopsworks.common.hdfs.HdfsUsersController;
 import io.hops.hopsworks.common.util.Settings;
 import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.hadoop.fs.FSDataInputStream;
-
-import java.io.IOException;
-import java.io.DataInputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.File;
 
 import javax.ejb.EJB;
 import javax.ejb.TransactionAttribute;
@@ -98,20 +91,13 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
-import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.SecurityContext;
 
 @RequestScoped
 @TransactionAttribute(TransactionAttributeType.NEVER)
