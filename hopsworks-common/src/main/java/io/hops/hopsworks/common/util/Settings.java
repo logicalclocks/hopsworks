befea835ded9cdcb51905174e5945cbc70a4257b
Kim Hammar <kimham@kth.se>
2019-02-19 15:58:33 +0100

    test

--- hopsworks-common/src/main/java/io/hops/hopsworks/common/util/Settings.java
+++ hopsworks-common/src/main/java/io/hops/hopsworks/common/util/Settings.java
@@ -558,6 +558,7 @@ public class Settings implements Serializable {
           applicationCertificateValidityPeriod);
       tensorBoardMaxLastAccessed = setIntVar(TENSORBOARD_MAX_LAST_ACCESSED, tensorBoardMaxLastAccessed);
       sparkUILogsOffset = setIntVar(SPARK_UI_LOGS_OFFSET, sparkUILogsOffset);
+      jupyterShutdownTimerInterval = setStrVar(JUPYTER_SHUTDOWN_TIMER_INTERVAL, jupyterShutdownTimerInterval);
 
       populateDelaCache();
       populateLDAPCache();
@@ -1597,8 +1598,6 @@ public class Settings implements Serializable {
     return ZEPPELIN_SYNC_INTERVAL;
   }
 
-  public static final int JUPYTER_PORT = 8888;
-
   // Jupyter
   private String JUPYTER_DIR = "/srv/hops/jupyter";
 
@@ -1606,7 +1605,16 @@ public class Settings implements Serializable {
     checkCache();
     return JUPYTER_DIR;
   }
- 
+
+  // Service key rotation interval
+  private static final String JUPYTER_SHUTDOWN_TIMER_INTERVAL = "jupyter_shutdown_timer_interval";
+  private String jupyterShutdownTimerInterval = "30m";
+
+  public synchronized String getJupyterShutdownTimerInterval() {
+    checkCache();
+    return jupyterShutdownTimerInterval;
+  }
+
   private String KAFKA_USER = "kafka";
 
   public synchronized String getKafkaUser() {
