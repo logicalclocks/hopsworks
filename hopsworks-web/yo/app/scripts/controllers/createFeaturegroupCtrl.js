1bdebf0528728214903d69d23abe0a9a9e8e6b8c
Kim Hammar <kimham@kth.se>
2019-04-05 10:03:52 +0200

    [HOPSWORKS-827] Append Migrate HopsUtil and hops-util-py to using jwt (#178)

--- hopsworks-web/yo/app/scripts/controllers/createFeaturegroupCtrl.js
+++ hopsworks-web/yo/app/scripts/controllers/createFeaturegroupCtrl.js
@@ -189,7 +189,7 @@ angular.module('hopsWorksApp')
                     "description": self.featuregroupDoc,
                     "features": self.features,
                     "version": 1,
-                    "featureCorrelationMatrixBase64": null,
+                    "featureCorrelationMatrix": null,
                     "descriptiveStatistics": null,
                     "updateMetadata": false,
                     "updateStats": false,
