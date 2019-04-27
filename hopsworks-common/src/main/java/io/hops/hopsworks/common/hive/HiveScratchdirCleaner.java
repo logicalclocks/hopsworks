88b631c29ebdb4c2cdfab769974a70e2f351976b
Fabio Buso <buso.fabio@gmail.com>
2019-04-27 16:54:21 +0200

    SparkSQL creates scratchdir as well

--- hopsworks-common/src/main/java/io/hops/hopsworks/common/hive/HiveScratchdirCleaner.java
+++ hopsworks-common/src/main/java/io/hops/hopsworks/common/hive/HiveScratchdirCleaner.java
@@ -42,6 +42,7 @@ import javax.ejb.TimerService;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.List;
@@ -73,8 +74,8 @@ public class HiveScratchdirCleaner {
 
   @PostConstruct
   private void init() {
-    applicationTypeSet = new HashSet<>();
-    applicationTypeSet.add("TEZ");
+    // SparkSQL creates a scratchdir as well
+    applicationTypeSet = new HashSet<>(Arrays.asList("TEZ", "SPARK"));
 
     applicationStateEnumSet = EnumSet.of(YarnApplicationState.NEW, YarnApplicationState.GENERATING_SECURITY_MATERIAL,
         YarnApplicationState.NEW_SAVING, YarnApplicationState.SUBMITTED, YarnApplicationState.ACCEPTED,
@@ -93,11 +94,15 @@ public class HiveScratchdirCleaner {
   @Timeout
   @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
   public void doCleanUp() {
-    YarnClientWrapper yarnClientWrapper = yarnService.getYarnClientSuper(settings.getConfiguration());
-    YarnClient yarnClient = yarnClientWrapper.getYarnClient();
-
-    DistributedFileSystemOps dfso = dfs.getDfsOps();
+    YarnClientWrapper yarnClientWrapper = null;
+    DistributedFileSystemOps dfso = null;
     try {
+      // This can throw a runtime exception. After 3 exceptions are thrown, the bean is not executed anymore.
+      // Wrap everything into the try to avoid it.
+      yarnClientWrapper = yarnService.getYarnClientSuper(settings.getConfiguration());
+      YarnClient yarnClient = yarnClientWrapper.getYarnClient();
+
+      dfso = dfs.getDfsOps();
 
       Set<String> queueSet = new HashSet<>();
       List<QueueInfo> queueInfoList = yarnClient.getAllQueues();
@@ -112,7 +117,9 @@ public class HiveScratchdirCleaner {
       List<Inode> scratchDirs = inodeFacade.getChildren(scratchDirParent);
 
       for (Inode scratchDir : scratchDirs) {
-        if (scratchDir.getModificationTime().longValue() < threshold){
+        // Special case for the Hive superuser.
+        if (scratchDir.getModificationTime().longValue() < threshold
+            && !scratchDir.getInodePK().getName().equals(settings.getHiveSuperUser())){
           /*
            * Directories are in the format `scratchDirParent`/`hiveUser`
            *
@@ -135,7 +142,7 @@ public class HiveScratchdirCleaner {
                 applicationTypeSet, applicationStateEnumSet);
 
             if (appReports.isEmpty()) {
-              // No Hive query is running for this user. Safe to delete.
+              // No Hive query/SparkSQL is running for this user. Safe to delete.
 
               // Delete as superupser as the user might have been removed from the system
               // So we don't have the certificates
@@ -147,7 +154,7 @@ public class HiveScratchdirCleaner {
           }
         }
       }
-    } catch (YarnException | IOException e) {
+    } catch (Exception e) {
       logger.log(Level.SEVERE, "Error while starting Hive scratchdir cleaning ", e);
     } finally {
       yarnService.closeYarnClient(yarnClientWrapper);
