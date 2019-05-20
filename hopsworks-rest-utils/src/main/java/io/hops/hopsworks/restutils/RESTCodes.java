5c2b4543f849b6272544fcf712a1f3a6ea5797e9
Ermias <ermiasg@kth.se>
2019-05-20 14:47:14 +0200

    fix error code

--- hopsworks-rest-utils/src/main/java/io/hops/hopsworks/restutils/RESTCodes.java
+++ hopsworks-rest-utils/src/main/java/io/hops/hopsworks/restutils/RESTCodes.java
@@ -183,7 +183,7 @@ public class RESTCodes {
     QUOTA_REQUEST_NOT_COMPLETE(72, "Please specify both " + "namespace and space quota.", Response.Status.BAD_REQUEST),
     RESERVED_PROJECT_NAME(73, "Not allowed - reserved project name, pick another project name.",
         Response.Status.BAD_REQUEST),
-    PROJECT_ANACONDA_ENABLE_ERROR(71, "Failed to enable conda.", Response.Status.INTERNAL_SERVER_ERROR);
+    PROJECT_ANACONDA_ENABLE_ERROR(74, "Failed to enable conda.", Response.Status.INTERNAL_SERVER_ERROR);
 
 
     private Integer code;
