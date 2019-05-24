89fca880c58ce8e30972a7ef8b9a4468bac6202e
Ermias <ermiasg@kth.se>
2019-05-24 12:00:24 +0200

    improve project test fix

--- hopsworks-IT/src/test/ruby/spec/projects_spec.rb
+++ hopsworks-IT/src/test/ruby/spec/projects_spec.rb
@@ -255,7 +255,7 @@ describe "On #{ENV['OS']}" do
         end
         it "should delete project" do
           # Start Jupyter to put X.509 to HDFS
-          create_env(@project, "3.6", true)
+          @project = create_env_and_update_project(@project, "3.6", true)
           get "#{ENV['HOPSWORKS_API']}/project/#{@project[:id]}/jupyter/settings"
           expect_status(200)
           settings = json_body
