34657a981431eafb0f17cd7952368ccd73cac3fb
robzor <robin.eric.andersson@gmail.com>
2019-07-26 12:04:48 +0200

    [HOPSWORKS-1181] Remove hopsfacets  (#291)

--- hopsworks-common/src/main/java/io/hops/hopsworks/common/util/Settings.java
+++ hopsworks-common/src/main/java/io/hops/hopsworks/common/util/Settings.java
@@ -3020,7 +3020,7 @@ public class Settings implements Serializable {
   private Set<String> PROVIDED_PYTHON_LIBRARY_NAMES;
   private static final String VARIABLE_PROVIDED_PYTHON_LIBRARY_NAMES = "provided_python_lib_names";
   private static final String DEFAULT_PROVIDED_PYTHON_LIBRARY_NAMES =
-      "hops, pandas, tensorflow-serving-api, hopsfacets, mmlspark, numpy";
+      "hops, pandas, tensorflow-serving-api, mmlspark, numpy";
 
   public synchronized Set<String> getProvidedPythonLibraryNames() {
     checkCache();
