b9c503518288aa1a0657d4db1892a3e8e328ff42
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2019-08-29 14:14:48 +0200

    [HOPSWORKS-1243] Jupyter Git support for CE

---
+++ hopsworks-common/src/main/java/io/hops/hopsworks/common/jupyter/JupyterNbVCSController.java
@@ -0,0 +1,35 @@
+/*
+ * This file is part of Hopsworks
+ * Copyright (C) 2019, Logical Clocks AB. All rights reserved
+ *
+ * Hopsworks is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Hopsworks is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.hops.hopsworks.common.jupyter;
+
+import io.hops.hopsworks.common.dao.jupyter.JupyterProject;
+import io.hops.hopsworks.common.dao.user.Users;
+import io.hops.hopsworks.exceptions.ServiceException;
+
+import java.util.Set;
+
+public interface JupyterNbVCSController {
+  boolean isGitAvailable();
+  JupyterContentsManager getJupyterContentsManagerClass(String remoteURI) throws ServiceException;
+  Set<String> getRemoteBranches(Users user, String apiKeyName, String remoteURI) throws ServiceException;
+  String getGitApiKey(String hdfsUser, String apiKeyName) throws ServiceException;
+  
+  RepositoryStatus cloneOrPullRemoteRepository(JupyterProject jupyterProject) throws ServiceException;
+  RepositoryStatus commit(JupyterProject jupyterProject, Users user, String commitMessage) throws ServiceException;
+  RepositoryStatus push(JupyterProject jupyterProject) throws ServiceException;
+  RepositoryStatus status(JupyterProject jupyterProject) throws ServiceException;
+}
