c48a79e62e2390e6e4566769319e8203c661eb4e
Steffen Grohsschmiedt <steffen@logicalclocks.com>
2019-09-24 16:47:39 +0200

    [HOPSWORKS-1293] Add credential endpoint
    Add an endpoint to download the project user credentials provided
    an API key. This enables accessing Hive and similar services
    from outside without the need to set up credentials multiple times.

---
+++ hopsworks-common/src/main/java/io/hops/hopsworks/common/project/AccessCredentialsDTO.java
@@ -0,0 +1,73 @@
+/*
+ * This file is part of Hopsworks
+ * Copyright (C) 2019, Logical Clocks AB. All rights reserved
+ *
+ * Hopsworks is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Hopsworks is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.hops.hopsworks.common.project;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class AccessCredentialsDTO {
+  private String fileExtension;
+  private String kStore;
+  private String tStore;
+  private String password;
+
+  public AccessCredentialsDTO() {
+  }
+
+  public AccessCredentialsDTO(String fileExtension) {
+    this.fileExtension = fileExtension;
+  }
+
+  public AccessCredentialsDTO(String fileExtension, String kStore, String tStore, String password) {
+    this.fileExtension = fileExtension;
+    this.kStore = kStore;
+    this.tStore = tStore;
+    this.password = password;
+  }
+
+  public String getFileExtension() {
+    return fileExtension;
+  }
+
+  public void setFileExtension(String fileExtension) {
+    this.fileExtension = fileExtension;
+  }
+
+  public String getkStore() {
+    return kStore;
+  }
+
+  public void setkStore(String kStore) {
+    this.kStore = kStore;
+  }
+
+  public String gettStore() {
+    return tStore;
+  }
+
+  public void settStore(String tStore) {
+    this.tStore = tStore;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
