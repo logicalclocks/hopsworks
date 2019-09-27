825b78afa530cff177adffa86704bb220073d086
Mahmoud Ismail <mahmoud@logicalclocks.com>
2019-09-27 11:07:04 +0200

    [HOPSWORKS-1027] Add support for multiple elasticsearch nodes (HA)

---
+++ hopsworks-common/src/main/java/io/hops/hopsworks/common/util/ElasticSettings.java
@@ -0,0 +1,68 @@
+/*
+ * This file is part of Hopsworks
+ * Copyright (C) 2019, Logical Clocks AB. All rights reserved
+ *
+ * Hopsworks is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Hopsworks is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+package io.hops.hopsworks.common.util;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ElasticSettings {
+  public final static String ELASTIC_IP_DEFAULT="127.0.0.1";
+  public final static int ELASTIC_PORT_DEFAULT = 9300;
+  public final static int ELASTIC_REST_PORT_DEFAULT = 9200;
+  
+  private final List<String> elasticIps;
+  private final int elasticPort;
+  private final int elasticRestPort;
+  
+  private int rrIndex = 0;
+  
+  public ElasticSettings(String elasticIps, int elasticPort, int elasticRestPort){
+    String[] ips = elasticIps.split(",");
+    List<String> validIps = new ArrayList<>();
+    for (String ip : ips) {
+      if (Ip.validIp(ip)) {
+        validIps.add(ip);
+      }
+    }
+    this.elasticIps = validIps;
+    this.elasticPort = elasticPort;
+    this.elasticRestPort = elasticRestPort;
+  }
+  
+  public List<String> getElasticIps() {
+    return elasticIps;
+  }
+  
+  public String getElasticEndpoint() {
+    return getElasticIp() + ":" + elasticPort;
+  }
+  
+  public String getElasticRESTEndpoint() {
+    return getElasticIp() + ":" + elasticRestPort;
+  }
+  
+  public int getElasticPort() {
+    return elasticPort;
+  }
+  
+  private String getElasticIp(){
+    if(rrIndex == elasticIps.size()){
+      rrIndex = 0;
+    }
+    return elasticIps.get(rrIndex++);
+  }
+}
