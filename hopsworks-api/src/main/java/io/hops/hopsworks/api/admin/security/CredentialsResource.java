64eb9f0a5d1718c8f8204951ccfd23388f5d383b
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2019-11-19 11:07:49 +0100

    [HOPSWORKS-1362] Add endpoint for platform admins or agents to get users' x.509 credentials. Required by WebHDFS (#362)

---
+++ hopsworks-api/src/main/java/io/hops/hopsworks/api/admin/security/CredentialsResource.java
@@ -0,0 +1,37 @@
+/*
+ * This file is part of Hopsworks
+ * Copyright (C) 2019, Logical Clocks AB. All rights reserved
+ *
+ * Hopsworks is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Hopsworks is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.hops.hopsworks.api.admin.security;
+
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Path;
+
+@RequestScoped
+@Path("/admin/credentials")
+@TransactionAttribute(TransactionAttributeType.NEVER)
+public class CredentialsResource {
+  
+  @Inject
+  private X509Resource x509Resource;
+  
+  @Path("/x509")
+  public X509Resource getX509Credentials() {
+    return x509Resource;
+  }
+}
