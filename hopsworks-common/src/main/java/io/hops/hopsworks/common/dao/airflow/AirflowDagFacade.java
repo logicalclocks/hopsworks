8a861abd90dbe8e68bbfa3f70fd7ebabdc381e8e
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2020-01-23 11:58:23 +0100

    [HOPSWORKS-1551] Do not use a Persistence Unit for the optional Airflow database (#464)

--- hopsworks-common/src/main/java/io/hops/hopsworks/common/dao/airflow/AirflowDagFacade.java
+++ hopsworks-common/src/main/java/io/hops/hopsworks/common/dao/airflow/AirflowDagFacade.java
@@ -16,33 +16,81 @@
 
 package io.hops.hopsworks.common.dao.airflow;
 
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * This is special case of a facade. It does not use the EntityManager.
+ *
+ * Installation of Airflow is optional. This means that we can't have the
+ * airflow persistence unit statically in persistence.xml file as the Connection
+ * Pool and JDBC Resource won't exist in Glassfish.
+ *
+ * All persistence units in persistence.xml *must* exist during application
+ * deployment. Otherwise the deployment fails.
+ *
+ * To tackle this dynamic installation, we skip adding airflow PU in persistence.xml
+ * and we inject the JDBC Resource programmatically.
+ *
+ * The method in this EJB will only be invoked when Airflow is installed, so it is
+ * safe to inject jdbc/airflow DataSource.
+ *
+ * DO NOT invoke the DataSource directly!
+ *
+ * Look on other facades for examples on how to properly access the database.
+ */
 
 @Stateless
-@TransactionAttribute(TransactionAttributeType.REQUIRED)
+@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
 public class AirflowDagFacade {
   
-  @PersistenceContext(unitName = "airflowPU")
-  private EntityManager em;
+  private static final Logger LOGGER = Logger.getLogger(AirflowDagFacade.class.getName());
+  private static final String DAGS_STATUS_QUERY = "SELECT dag_id, is_paused FROM airflow.dag WHERE owners = ?";
   
-  public List<AirflowDag> getAll() {
-    TypedQuery<AirflowDag> query = em.createNamedQuery("AirflowDag.getAll", AirflowDag.class);
-    return query.getResultList();
-  }
+  @Resource(name = "jdbc/airflow")
+  private DataSource airflowDataSource;
   
-  public List<AirflowDag> filterByOwner(String owner) throws IOException {
+  public List<AirflowDag> filterByOwner(String owner) throws IOException, SQLException {
     if (owner == null || owner.isEmpty()) {
       throw new IOException("Airflow DAG owner cannot be null or empty");
     }
-    return em.createNamedQuery("AirflowDag.filterByOwner", AirflowDag.class)
-        .setParameter("owner", owner)
-        .getResultList();
+    List<AirflowDag> dags = new ArrayList<>();
+    PreparedStatement stmt = null;
+    ResultSet dagsRS = null;
+    try {
+      Connection connection = airflowDataSource.getConnection();
+      stmt = connection.prepareStatement(DAGS_STATUS_QUERY);
+      stmt.setString(1, owner);
+      dagsRS = stmt.executeQuery();
+      while (dagsRS.next()) {
+        AirflowDag dag = new AirflowDag(dagsRS.getString("dag_id"), dagsRS.getBoolean("is_paused"));
+        dags.add(dag);
+      }
+      return dags;
+    } finally {
+      try {
+        if (dagsRS != null) {
+          dagsRS.close();
+        }
+        if (stmt != null) {
+          stmt.close();
+        }
+      } catch (SQLException ex) {
+        // Just log them, can't do much about them here
+        LOGGER.log(Level.WARNING, "Could not release resources", ex);
+      }
+    }
   }
 }
