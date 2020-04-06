af3d829fb2ba8f5585ba01038b8f38800b2b8bee
Theofilos Kakantousis <tkakantousis@users.noreply.github.com>
2020-04-06 12:42:50 +0200

    [HOPSWORKS-1696] Increase timeout for listing conda libs when creating a project (#517)

--- hopsworks-common/src/main/java/io/hops/hopsworks/common/agent/AgentController.java
+++ hopsworks-common/src/main/java/io/hops/hopsworks/common/agent/AgentController.java
@@ -59,6 +59,7 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -320,6 +321,7 @@ public class AgentController {
     ProcessDescriptor processDescriptor = new ProcessDescriptor.Builder()
         .addCommand(prog)
         .addCommand(project)
+        .setWaitTimeout(60, TimeUnit.SECONDS)
         .build();
 
     try {
