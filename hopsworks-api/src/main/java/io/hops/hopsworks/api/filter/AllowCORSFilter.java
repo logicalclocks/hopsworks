5e63fe95978a549f880e04ac5e7bcc7e56c7f8c7
Fabio Buso <fabio@logicalclocks.com>
2020-11-23 10:37:29 +0100

    Add Access-Control-Expose-Headers to CORS filter headers (#360)

--- hopsworks-api/src/main/java/io/hops/hopsworks/api/filter/AllowCORSFilter.java
+++ hopsworks-api/src/main/java/io/hops/hopsworks/api/filter/AllowCORSFilter.java
@@ -28,6 +28,7 @@ public class AllowCORSFilter implements ContainerResponseFilter {
   public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
     response.getHeaders().add("Access-Control-Allow-Origin", "*");
     response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+    response.getHeaders().add("Access-Control-Expose-Headers", "authorization");
     response.getHeaders().add("Access-Control-Allow-Credentials", "true");
     response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
   }
--- hopsworks-ca/src/main/java/io/hops/hopsworks/ca/api/filter/CORSFilter.java
+++ hopsworks-ca/src/main/java/io/hops/hopsworks/ca/api/filter/CORSFilter.java
@@ -58,6 +58,7 @@ public class CORSFilter implements ContainerResponseFilter {
         responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add("Access-Control-Expose-Headers", "authorization");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
         responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
       }
