e0b19de265f141f6c94841fdcd1f09e9186048a3
Gibson Chikafa <gibbschikafa@gmail.com>
2020-12-07 17:44:15 +0100

    [HOPSWORKS-2102][append] Create python job fix (#386)

--- hopsworks-web/yo/app/scripts/controllers/newJobCtrl.js
+++ hopsworks-web/yo/app/scripts/controllers/newJobCtrl.js
@@ -704,7 +704,7 @@ angular.module('hopsWorksApp')
                         }
                         break;
                       case "PYTHON":
-                        self.mainFileSelected(filename);
+                        self.mainFileSelected(path);
                         break;
                       default:
                         break;
