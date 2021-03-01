d5cb3c497d621d13439f6918c0c787cde93782a1
Fabio Buso <buso.fabio@gmail.com>
2021-03-01 22:42:20 +0100

    [HOPSWORKS-2139] Fix build

--- hopsworks-common/src/main/java/io/hops/hopsworks/common/featurestore/featuregroup/cached/FeatureGroupCommitFacade.java
+++ hopsworks-common/src/main/java/io/hops/hopsworks/common/featurestore/featuregroup/cached/FeatureGroupCommitFacade.java
@@ -26,7 +26,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import javax.persistence.TypedQuery;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.Optional;
