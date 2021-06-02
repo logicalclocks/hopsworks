f4ca885316854266808e8b4c8cd6cc2679860c65
robzor92 <robin.eric.andersson@gmail.com>
2021-06-02 16:16:38 +0200

    [HOPSWORKS-2594] Attaching Jupyter config when running notebook with Spark or Python kernel with git backend fails

--- hopsworks-common/src/main/java/io/hops/hopsworks/common/jupyter/JupyterController.java
+++ hopsworks-common/src/main/java/io/hops/hopsworks/common/jupyter/JupyterController.java
@@ -348,9 +348,13 @@ public class JupyterController {
       throws ServiceException {
     DistributedFileSystemOps udfso = null;
     try {
+      JupyterSettings jupyterSettings = jupyterSettingsFacade.findByProjectUser(project, user.getEmail());
+      //If Jupyter was started with a git backend, do not attach the xattribute
+      if(jupyterSettings.isGitBackend()) {
+        return;
+      }
       udfso = dfs.getDfsOps(hdfsUsername);
       String relativeNotebookPath = getNotebookRelativeFilePath(hdfsUsername, kernelId, udfso);
-      JupyterSettings jupyterSettings = jupyterSettingsFacade.findByProjectUser(project, user.getEmail());
       ObjectMapper objectMapper = new ObjectMapper();
       objectMapper.registerModule(new JaxbAnnotationModule());
       JSONObject jupyterSettingsMetadataObj = new JSONObject();
