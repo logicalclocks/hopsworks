5e7172bdafbc0f8d34b9d0544bf98e37b2439be2
Ermias <ermiasg@kth.se>
2021-08-12 09:17:36 +0200

    fix license

--- hopsworks-api/src/main/java/io/hops/hopsworks/api/project/alert/ProjectAllAlertsDTO.java
+++ hopsworks-api/src/main/java/io/hops/hopsworks/api/project/alert/ProjectAllAlertsDTO.java
@@ -1,8 +1,26 @@
+/*
+ * This file is part of Hopsworks
+ * Copyright (C) 2021, Logical Clocks AB. All rights reserved
+ *
+ * Hopsworks is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU Affero General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Hopsworks is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
 package io.hops.hopsworks.api.project.alert;
 
 import io.hops.hopsworks.api.featurestore.datavalidation.alert.FeatureGroupAlertDTO;
 import io.hops.hopsworks.api.jobs.alert.JobAlertsDTO;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class ProjectAllAlertsDTO {
   private ProjectAlertsDTO projectAlerts;
   private JobAlertsDTO jobAlerts;
@@ -31,8 +49,7 @@ public class ProjectAllAlertsDTO {
     return featureGroupAlerts;
   }
 
-  public void setFeatureGroupAlerts(
-      FeatureGroupAlertDTO featureGroupAlerts) {
+  public void setFeatureGroupAlerts(FeatureGroupAlertDTO featureGroupAlerts) {
     this.featureGroupAlerts = featureGroupAlerts;
   }
 }
