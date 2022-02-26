581ba824c23b41d000a4d4f7b6ff9a0035aa2d67
Jim Dowling <jim@logicalclocks.com>
2022-02-26 12:17:33 +0100

    add script to disable cors (#827)
    * add script to disable and re-enable cors
    Co-authored-by: Alex Ormenisan <ormenisan.adrian@gmail.com>

---
+++ scripts/undo-cors.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sed -i.bak -e 's@^     register(io\.hops\.hopsworks\.filters\.AllowCORSFilter@    // register(io\.hops\.hopsworks\.filters\.AllowCORSFilter@g' ./hopsworks-api/src/main/java/io/hops/hopsworks/rest/application/config/ApplicationConfig.java
+sed -i.bak -e 's@^     register(io\.hops\.hopsworks\.filters\.AllowCORSFilter@    //register(io\.hops\.hopsworks\.filters\.AllowCORSFilter@g' ./hopsworks-remote-user-api/src/main/java/io/hops/hopsworks/remote/user/rest/application/config/ApplicationConfig.java
+
+rm -f ./hopsworks-api/src/main/java/io/hops/hopsworks/rest/application/config/ApplicationConfig.java.bak
+rm -f ./hopsworks-remote-user-api/src/main/java/io/hops/hopsworks/remote/user/rest/application/config/ApplicationConfig.java.bak
