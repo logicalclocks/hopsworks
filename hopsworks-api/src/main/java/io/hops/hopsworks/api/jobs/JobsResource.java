71bcb7ec4fd661cc521151401ef21778f4fe9524
Ermias <ermiasg@kth.se>
2022-06-14 18:57:09 +0200

    job config and create opensearch jwt

--- hopsworks-api/src/main/java/io/hops/hopsworks/api/jobs/JobsResource.java
+++ hopsworks-api/src/main/java/io/hops/hopsworks/api/jobs/JobsResource.java
@@ -293,8 +293,9 @@ public class JobsResource {
   @Produces(MediaType.APPLICATION_JSON)
   @Consumes(MediaType.APPLICATION_JSON)
   @AllowedProjectRoles({AllowedProjectRoles.DATA_OWNER, AllowedProjectRoles.DATA_SCIENTIST})
-  @JWTRequired(acceptedTokens={Audience.API, Audience.JOB}, allowedUserRoles={"HOPS_ADMIN", "HOPS_USER"})
-  @ApiKeyRequired( acceptedScopes = {ApiScope.JOB}, allowedUserRoles = {"HOPS_ADMIN", "HOPS_USER"})
+  @JWTRequired(acceptedTokens = {Audience.API, Audience.JOB},
+    allowedUserRoles = {"HOPS_ADMIN", "HOPS_USER", "HOPS_SERVICE_USER"})
+  @ApiKeyRequired(acceptedScopes = {ApiScope.JOB}, allowedUserRoles = {"HOPS_ADMIN", "HOPS_USER", "HOPS_SERVICE_USER"})
   public Response configuration (@ApiParam (value = "job type", example = "spark")
                                  @PathParam("jobtype") JobType jobtype,
                                  @Context SecurityContext sc) throws JobException {
