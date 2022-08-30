4e3a354cd7ee132fa4f282c7413917f3443f0de9
Antonis Kouzoupis <antonios@logicalclocks.com>
2022-08-30 15:57:32 +0200

    [HOPSWORKS-3314] Allow API key as authentication method (#1161) (#1163)

--- hopsworks-api/src/main/java/io/hops/hopsworks/api/util/ClusterUtilisationService.java
+++ hopsworks-api/src/main/java/io/hops/hopsworks/api/util/ClusterUtilisationService.java
@@ -42,10 +42,12 @@ package io.hops.hopsworks.api.util;
 import com.logicalclocks.servicediscoverclient.exceptions.ServiceDiscoveryException;
 import com.logicalclocks.servicediscoverclient.service.Service;
 import io.hops.hopsworks.api.filter.Audience;
+import io.hops.hopsworks.api.filter.apiKey.ApiKeyRequired;
 import io.hops.hopsworks.common.hosts.ServiceDiscoveryController;
 import io.hops.hopsworks.common.proxies.client.HttpClient;
 import io.hops.hopsworks.exceptions.ServiceException;
 import io.hops.hopsworks.jwt.annotation.JWTRequired;
+import io.hops.hopsworks.persistence.entity.user.security.apiKey.ApiScope;
 import io.hops.hopsworks.restutils.RESTCodes;
 import io.swagger.annotations.Api;
 import org.apache.http.HttpHost;
@@ -67,6 +69,7 @@ import javax.ws.rs.core.Response;
 @Stateless
 @Path("/clusterUtilisation")
 @JWTRequired(acceptedTokens = {Audience.API}, allowedUserRoles = {"HOPS_ADMIN", "HOPS_USER"})
+@ApiKeyRequired(acceptedScopes = {ApiScope.ADMIN}, allowedUserRoles = {"HOPS_ADMIN", "HOPS_USER"})
 @Api(value = "Cluster Utilisation Service", description = "Cluster Utilisation Service")
 @TransactionAttribute(TransactionAttributeType.NEVER)
 public class ClusterUtilisationService {
