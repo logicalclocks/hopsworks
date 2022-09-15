88c652eb2f722831a292e264fb25316da18d9f84
Robin Andersson <robin.eric.andersson@gmail.com>
2022-09-15 13:16:39 +0200

    [HOPSWORKS-3346] LatestVersionAnalyzer fails to find the latest version due to change in version formats including RC (#1171)

--- hopsworks-common/src/main/java/io/hops/hopsworks/common/python/updates/analyzer/HsmlLatestVersionAnalyzer.java
+++ hopsworks-common/src/main/java/io/hops/hopsworks/common/python/updates/analyzer/HsmlLatestVersionAnalyzer.java
@@ -50,6 +50,8 @@ public class HsmlLatestVersionAnalyzer extends LatestVersionAnalyzer {
 
       List<LibraryVersionDTO> searchVersionHits = versions.get(this.getLibrary());
 
+      searchVersionHits = super.filterReleases(searchVersionHits);
+
       String [] hopsworksVersionSplit = currentHopsworksVersion.split("\\.");
 
       String hsmlVersionPrefix = hopsworksVersionSplit[0] + "." + hopsworksVersionSplit[1];
