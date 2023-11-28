441e26fe0ab7a332cc54c7317ec5cbc52a62ca26
Ermias <ermiasg@kth.se>
2023-11-28 17:42:19 +0100

    fix for no user group view

--- docker/migration/upgrade.sh
+++ docker/migration/upgrade.sh
@@ -4,6 +4,12 @@ set -e
 
 MYSQL_CMD="mysql --host=${MYSQL_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD}"
 
+for filename in /flyway/updates/sql/*.sql; do
+  file=$(basename "${filename%.*}");
+  dir=$(dirname "$filename")
+  mv -f "$filename" "$dir/V${file}__initial_tables.sql"
+done
+
 echo "Running flyway migrate"
 flyway -configFiles=config/flyway.conf -locations=filesystem:/flyway/updates/sql -validateOnMigrate=false migrate
 
