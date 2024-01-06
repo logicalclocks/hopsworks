2c889ecb26f6626d422dbec786cdf944e0d7aacf
bubriks <bubriks@gmail.com>
2024-01-06 09:59:40 +0100

    fix NPE

--- hopsworks-api/src/main/java/io/hops/hopsworks/api/featurestore/featureview/FeatureViewInputValidator.java
+++ hopsworks-api/src/main/java/io/hops/hopsworks/api/featurestore/featureview/FeatureViewInputValidator.java
@@ -45,7 +45,7 @@ public class FeatureViewInputValidator {
 
   public void validate(FeatureViewDTO featureViewDTO, Project project, Users user) throws FeaturestoreException {
     featurestoreInputValidation.verifyUserInput(featureViewDTO);
-    validateCreationInput(featureViewDTO);
+    validateCreationInput(featureViewDTO.getQuery());
     Query query = queryController.convertQueryDTO(project, user, featureViewDTO.getQuery(), false);
     validateVersion(featureViewDTO.getVersion());
     trainingDatasetInputValidation.validateFeatures(query, featureViewDTO.getFeatures());
@@ -54,10 +54,19 @@ public class FeatureViewInputValidator {
     }
   }
 
-  public void validateCreationInput(FeatureViewDTO featureViewDTO) throws FeaturestoreException {
-    if (featureViewDTO.getQuery() == null) {
+  public void validateCreationInput(QueryDTO queryDTO) throws FeaturestoreException {
+    if (queryDTO == null) {
       throw new FeaturestoreException(RESTCodes.FeaturestoreErrorCode.FEATURE_VIEW_CREATION_ERROR, Level.FINE,
           "`Query` is missing from input.");
+    }
+    if (queryDTO.getLeftFeatures() == null || queryDTO.getLeftFeatures().isEmpty()) {
+      throw new FeaturestoreException(RESTCodes.FeaturestoreErrorCode.FEATURE_VIEW_CREATION_ERROR, Level.FINE,
+          "Queries must have features");
+    }
+    if (queryDTO.getJoins() != null) {
+      for (JoinDTO joinDTO : queryDTO.getJoins()) {
+        validateCreationInput(joinDTO.getQuery());
+      }
     }
   }
 
