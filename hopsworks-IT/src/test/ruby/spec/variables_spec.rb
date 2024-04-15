a5f5d5d7d93b43d5f3301a51b0b140ed91f80723
ErmiasG <ermiasg@kth.se>
2024-04-15 11:16:13 +0200

    [HWORKS-1001][HWORKS-973] Share default project (#1516)
    * HWORKS-1001 and HWORKS-973
    
    * remove ee test

--- hopsworks-IT/src/test/ruby/spec/variables_spec.rb
+++ hopsworks-IT/src/test/ruby/spec/variables_spec.rb
@@ -219,5 +219,56 @@ describe "On #{ENV['OS']}" do
         expect(parsed['devMsg']).to include("quota")
       end
     end
+
+    describe 'default feature store share' do
+      before(:all) do
+        with_valid_project
+        create_var('default_feature_store_project_id', @project[:id])
+        reset_session
+      end
+      after(:all) do
+        setVar('default_feature_store_project_id', '')
+        reset_session
+      end
+
+      it "should share feature store of default fs project" do
+        project = create_project
+        test_feature_store_shared(@project, project)
+      end
+    end
+    describe "with feature groups quota enabled" do
+      before :all do
+        setVar("quotas_featuregroups_online_disabled", "1")
+        setVar("quotas_featuregroups_online_enabled", "1")
+      end
+      after :all do
+        setVar("quotas_featuregroups_online_disabled", "-1")
+        setVar("quotas_featuregroups_online_enabled", "-1")
+      end
+      it "should fail to create cached feature groups if quota has been reached" do
+        ## Create new project
+        project = create_project
+        featurestore_id = get_featurestore_id(project.id)
+        ## First attempt should succeed
+        create_cached_featuregroup(project.id, featurestore_id, online: true)
+        expect_status_details(201)
+
+        ## This time is should fail because it has reached the online enabled limit
+        result, _ =create_cached_featuregroup(project.id, featurestore_id, online: true)
+        expect_status_details(500)
+        parsed = JSON.parse(result)
+        expect(parsed['devMsg']).to include("quota")
+
+        ## Online disabled should go through
+        create_cached_featuregroup(project.id, featurestore_id, online: false)
+        expect_status_details(201)
+
+        ## Now reached limit for online disabled too
+        result, _ = create_cached_featuregroup(project.id, featurestore_id, online: false)
+        expect_status_details(500)
+        parsed = JSON.parse(result)
+        expect(parsed['devMsg']).to include("quota")
+      end
+    end
   end
 end
